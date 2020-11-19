222e0e573838c68b8dc1b8be0a3cf7e2105d490c
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-11-19 15:40:35 -0600

    Anotaciones

--- 20_10_09-04-Gif-Expert-App/src/components/GifGridItem.js
+++ 20_10_09-04-Gif-Expert-App/src/components/GifGridItem.js
@@ -5,7 +5,7 @@ export const GifGridItem = ({title, url}) => { //** Estos son los parametros que
     return ( //** Es lo que retorna el arreglo de GifGridItem */
         // Estos son los estilos de bootstrap, y aqui vemos que solo se hace el llamado de los parametros dichos en el getGifs.js
         // Como lo vez aqui el div con el className se nos puso a modo de bootstrap, le ponemos el nombre, que en este caso es card y luego la animamos como queramos
-        <div className="card animate__animated animate__fadeIn">
+        <div className="card animate__animated animate__fadeIn"> 
             {/* Estos son los parametros que trae cada imagen */}
             <img src={url} alt={title}/> 
             {/* Es el titulo que va abajo */}
