dd92f03e76682799f0a557d17825243752a330a0
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-11-20 14:46:23 -0600

    Pruebas y Anotaciones

--- 20_10_09-04-Gif-Expert-App/src/tests/components/GifGridItem.test.js
+++ 20_10_09-04-Gif-Expert-App/src/tests/components/GifGridItem.test.js
@@ -4,12 +4,12 @@ import { GifGridItem } from '../../components/GifGridItem'
 
 describe('Pruebas en <GifGridItem />', () => { //** Al componente que le vamos a hacer la prueba */
 
-    // const title = "Un titulo";
-    // const url = 'http://localhost/algo.jpg';
+    const title = "Un titulo"; //** Como ahora le pusimos que isRequired estos son los argumentos que va a agarrar */
+    const url = 'http://localhost/algo.jpg'; //** Como ahora le pusimos que isRequired estos son los argumentos que va a agarrar */
 
     test('debe de mostrar el componente correctamente', () => {
         
-        const wrapper = shallow(<GifGridItem />) //** se crea una constante y GifGridItem es el sujeto de prueba y con esto se empieza a usar React, por eso lo importamos */
+        const wrapper = shallow(<GifGridItem title={title} url={url}/>) //** se crea una constante y GifGridItem es el sujeto de prueba y con esto se empieza a usar React, por eso lo importamos, pusimos el title y url porque lo metimos isRequired en la carpeta de GifGridItem */
 
         expect(wrapper).toMatchSnapshot(); //** Aqui vemos si el Snapchot es lo que el queria */
     })
