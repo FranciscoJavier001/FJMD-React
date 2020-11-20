922e0d823be2322308faeea12a504818e250d03f
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-11-20 14:30:50 -0600

    Pruebas y Anotaciones

--- 20_10_09-04-Gif-Expert-App/src/components/GifGridItem.js
+++ 20_10_09-04-Gif-Expert-App/src/components/GifGridItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 export const GifGridItem = ({title, url}) => { //** Estos son los parametros que se reciben en el map del GifGrid.js */
 
@@ -13,3 +14,16 @@ export const GifGridItem = ({title, url}) => { //** Estos son los parametros que
         </div>
     )
 }
+
+// GifGridItem.propTypes = {
+//     title: PropTypes.string.isRequired,
+//     url: PropTypes.string.isRequired,
+// }
+ /** 
+  * 1. Enzyme = Se hace la configuracion en el setupTests.js en terminal ponerle en la carpeta del proyecto "npm i --save-dev enzyme enzyme-adapter-react-16", luego agregarle la configuracion al setupTest que importa Enzyme, el Adapter y el Enzyme.configure({})
+  * 2. Enzyme to Json = Se configura en terminal con "npm install --save-dev enzyme-to-json", luego copiamos el import {createSerializer}, y el expect.addSnapshotSerializer en el setupTest
+  * 3. Debe de mostrar el componente correctamente = Se crea una nueva carpeta llamada tests>components (porque asi se llama a donde vamos a aplicar las pruebas)> y ahi meter las pruebas
+  * shallow = genera algo que se llama wrapper
+  * wrapper
+  * wrapper.toMatchSnapshot = debe de hacer match con el toMatchSnapshot()
+ */
--- 20_10_09-04-Gif-Expert-App/src/tests/components/GifGridItem.test.js
+++ 20_10_09-04-Gif-Expert-App/src/tests/components/GifGridItem.test.js
@@ -1,13 +1,17 @@
 import React from 'react'
-import {shallow} from 'enzyme'
+import {shallow} from 'enzyme' //** Este se importa de enzyme */
 import { GifGridItem } from '../../components/GifGridItem'
 
-describe('Pruebas en <GifGridItem />', () => {
+describe('Pruebas en <GifGridItem />', () => { //** Al componente que le vamos a hacer la prueba */
+
+    // const title = "Un titulo";
+    // const url = 'http://localhost/algo.jpg';
+
     test('debe de mostrar el componente correctamente', () => {
         
-        const wrapper = shallow(<GifGridItem/>)
+        const wrapper = shallow(<GifGridItem />) //** se crea una constante y GifGridItem es el sujeto de prueba y con esto se empieza a usar React, por eso lo importamos */
 
-        expect(wrapper).toMatchSnapshot();
+        expect(wrapper).toMatchSnapshot(); //** Aqui vemos si el Snapchot es lo que el queria */
     })
     
 })
