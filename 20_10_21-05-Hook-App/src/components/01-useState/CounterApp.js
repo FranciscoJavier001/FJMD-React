8d01ab68d183ab6e12992c9c030c52b20373174d
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-11-21 14:46:24 -0600

    Counter y Documentacion

--- 20_10_21-05-Hook-App/src/components/01-useState/CounterApp.js
+++ 20_10_21-05-Hook-App/src/components/01-useState/CounterApp.js
@@ -1,16 +1,18 @@
+// rafc porque es un Functional Component
+
 import React, { useState } from 'react';
 import './counter.css';
 
 export const CounterApp = () => {
 
-    const [ state, setState] = useState({
+    const [state, setState] = useState({ //** Creamos un State, donde le asignamos los valores */
         counter1: 10,
         counter2: 20,
         counter3: 30,
         counter4: 40,
     });
 
-    const { counter1, counter2 } = state;
+    const {counter1, counter2} = state; //** Aqui ya vienen desde el state, porque esta desestructurado (recuerda, podemos desestructurar cualquier objeto) y podemos acceder a sus propiedades */
 
     // console.log(counter)
 
@@ -24,8 +26,9 @@ export const CounterApp = () => {
             className="btn btn-primary"
             onClick={ () => {
                 setState({
-                    ...state,
-                    counter1: counter1 + 1
+                    ...state, //** Spred del estado actual */
+                    counter1: counter1 + 1, 
+                    // counter2: counter2 + 1, //** Para tomar los 2 */
                 });
             }}
         >
--- 20_10_21-05-Hook-App/src/index.js
+++ 20_10_21-05-Hook-App/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { HookApp } from './HookApp';
-// // import { CounterApp } from './components/01-useState/CounterApp';
+// import { HookApp } from './HookApp';
+import { CounterApp } from './components/01-useState/CounterApp';
 // // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // // import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
@@ -17,7 +17,7 @@ import { HookApp } from './HookApp';
 // import { MainApp } from './components/09-useContext/MainApp';
 
 ReactDOM.render( //** Esto ya viene en el Index, solo le cambio el nombre con el cual voy a trabajar */
-    <HookApp/>,
+    <CounterApp/>,
   document.getElementById('root')
 );
 
