edf989f77497fe21a91e528965ef4db39871db60
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-11-22 22:10:37 -0600

    useEffect Documentado

--- 20_10_21-05-Hook-App/src/components/02-useEffect/Message.js
+++ 20_10_21-05-Hook-App/src/components/02-useEffect/Message.js
@@ -18,7 +18,7 @@ export const Message = () => {
         return () => {
             window.removeEventListener('mousemove', mouseMove );
         }
-    }, [])
+    }, []) 
 
     return (
         <div>
