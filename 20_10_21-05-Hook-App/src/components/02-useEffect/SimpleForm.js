4e289dea67380f451aa49107d8393b15025268ca
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-11-22 21:35:30 -0600

    useEffect = Estar escuchando los cambios

--- 20_10_21-05-Hook-App/src/components/02-useEffect/SimpleForm.js
+++ 20_10_21-05-Hook-App/src/components/02-useEffect/SimpleForm.js
@@ -5,19 +5,18 @@ import './effects.css';
 
 export const SimpleForm = () => {
 
-
-    const [formState, setFormState] = useState({
+    const [formState, setFormState] = useState({ //** Voy a extraer las funciones desde aqui */
         name: '',
         email: ''
     });
 
-    const { name, email } = formState;
+    const { name, email } = formState; //** Aqui desestructuramos las propiedades del formState, que tienen ambas variables declaradas arriba */
 
-    useEffect( () => {
+    useEffect( () => { //** Es un Hook que nos permite activar un efecto secundario, cuando algo suceda en nuestros componentes, aqui le estamos diciendo.. dispara esto cuando tengas algun cambio */
         // console.log('hey!');
-    }, [] );
+    }, [] ); //** Le pasamos en el segundo parametro en donde queremos que se utilize */
 
-    useEffect( () => {
+    useEffect( () => { //** Estar ESCUCHANDO LOS CAMBIOS, (es un area de oportunidad) */
         // console.log('formState cambió');
     }, [formState] );
 
@@ -27,22 +26,24 @@ export const SimpleForm = () => {
 
 
 
-    const handleInputChange = ({ target }) => {
+    const handleInputChange = ({ target }) => { //** Se que el target ya esta desestructurado de 'e', es el evento que se dispara cuando hay algun cambio en name */
 
         setFormState({
-            ...formState,
-            [ target.name ]: target.value
+            ...formState, //** Lo mostramos por si hay propiedades que nos estan cambiando */
+            [ target.name ]: target.value //** Quiero que recibas el valor de name, y lo asignes ahora a target.value */
         });
+        // console.log(target.name); //** No se que onda, aqui nomas sale name */
+        console.log(target.value); //** Estas son las teclas que se precionan en cada momento */
 
     }
 
-
+    // Aqui se empieza a crear un formulario
     return (
         <>
             <h1>useEffect</h1>
             <hr />
-
             <div className="form-group">
+                {/* Esto es el cuadro de texto */}
                 <input 
                     type="text"
                     name="name"
@@ -50,11 +51,11 @@ export const SimpleForm = () => {
                     placeholder="Tu nombre"
                     autoComplete="off"
                     value={ name }
-                    onChange={ handleInputChange }
+                    onChange={ handleInputChange } 
                 />
             </div>
 
-
+            {/* Esto va a pasar cuando nos vallamos a hacer un evento, osea esto es como el rastreador de lo que hace cada cosa, es decir es la logica del campo, o de la accion */}
             <div className="form-group">
                 <input 
                     type="text"
@@ -66,7 +67,6 @@ export const SimpleForm = () => {
                     onChange={ handleInputChange }
                 />
             </div>
-
 
             { (name === '123') && <Message /> }
 
--- 20_10_21-05-Hook-App/src/index.js
+++ 20_10_21-05-Hook-App/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 
 // import { HookApp } from './HookApp';
 // import { CounterApp } from './components/01-useState/CounterApp';
-import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
-// // import { SimpleForm } from './components/02-useEffect/SimpleForm';
+// import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 // // import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 // // import { RealExampleRef } from './components/04-useRef/RealExampleRef';
@@ -17,7 +17,7 @@ import { CounterWithCustomHook } from './components/01-useState/CounterWithCusto
 // import { MainApp } from './components/09-useContext/MainApp';
 
 ReactDOM.render( //** Esto ya viene en el Index, solo le cambio el nombre con el cual voy a trabajar */
-    <CounterWithCustomHook/>,
+    <SimpleForm/>,
   document.getElementById('root')
 );
 
