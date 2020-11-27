9548f03870b15b4fe1ab83fce4cbe230124d1074
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-11-26 18:36:29 -0600

    useFetch, useRef y Documentacion

--- 20_10_21-05-Hook-App/src/components/04-useRef/RealExampleRef.js
+++ 20_10_21-05-Hook-App/src/components/04-useRef/RealExampleRef.js
@@ -5,19 +5,21 @@ import '../02-useEffect/effects.css';
 
 export const RealExampleRef = () => {
 
-    const [show, setShow] = useState(false);
+    const [show, setShow] = useState(false); //** Creamos un estado con el snipet que se va a llamar show y va a iniciar en false y este show lo que va a hacer es un valor booleano para mostrar conicionalmente el componente definido abajo */
 
     return (
         <div>
             <h1>RealExampleRef</h1>
             <hr />
 
-            { show && <MultipleCustomHooks /> }
+            {/* Es para renderizar un componente, asi que lo usamos desde el ejercicio anterior de las citas de breaking bad */}
+            {show && <MultipleCustomHooks/>}
+            {/* La instruccion puesta arriba dice, si show esta en true, entonces muestra el MultipleCustomHooks */}
 
-            <button
+            <button //** Aqui creamos el boton que le damos clases con bootstrap y con el onClick cambiamos el valor booleano de setShow para que haga el cambio del valor y este se vea reflejado en el useState o el estado del componente */
                 className="btn btn-primary mt-5"
-                onClick={ () => {
-                    setShow( !show );
+                onClick={ () => { //** Al hacer click setShow va a cambiar su valor a negativo, como es un tipo de dato tiene booleano y cambia estado/state */
+                    setShow( !show ); //** Va a ser igual al valor opuesto del show */
                 }}
             >
                 Show/Hide
--- 20_10_21-05-Hook-App/src/hooks/useFetch.js
+++ 20_10_21-05-Hook-App/src/hooks/useFetch.js
@@ -2,10 +2,10 @@ import { useState, useEffect, useRef } from 'react';
 
 export const useFetch = ( url ) => { //** Aqui pedimos el url, y cuando lo recibimos que haga algo */
     
-    const isMounted = useRef(true);
-    const [state, setState] = useState({data: null, loading: true, error: null}); //** Este es el estado del state */
+    const isMounted = useRef(true); //** El componente esta montado, porque efectivamente esta en true, y se esta renderizando la primera vez , que mantenga la referencia, para saber si el hook  */
+    const [state, setState] = useState({data: null, loading: true, error: null}); //** Este es el estado del state, y con una peticion asincrona cambiamos el estado */
 
-    useEffect( () => {
+    useEffect( () => { //** Solo hazlo cuando se carge por primera vez, es un seguro al programa (1), pero cuando se desmonte, osea que haga el return el efecto se desmonte , entonces quiero cambiar el valor de ese isMountea.current = falso, hasta que lo vuelvan a llamar */
         return () => {
             isMounted.current = false;
         }
@@ -20,19 +20,19 @@ export const useFetch = ( url ) => { //** Aqui pedimos el url, y cuando lo recib
             .then( resp => resp.json() ) //** Responde con una promesa y vamos a extraer el json (que se convierte en un string) */
             .then( data => { //** Esto devuelve otra promesa donde ya tenemos la data, y esta data es la respuesta del endPoint */
 
-                if ( isMounted.current ) {
+                // Las instrucciones de esto (2) aqui vemos como se le asignan las instrucciones a isMounted.current, osea los estados de este, uno nos dice, pero si me manda un error, cuando la data sea null, digamos que no se pudo cargar, osea la data es donde vemos si viene con info o no.
+                if ( isMounted.current ) { 
                     setState({ //** Ahora esto se va a cambiar cuando reciba algo */
                         loading: false,
                         error: null,
                         data
                     });
                 }
-
             })
             .catch( () => {
                 setState({
-                    data: null,
                     loading: false,
+                    data: null,
                     error: 'No se pudo cargar la info'
                 })
             })
