8c40bc47512e03bdbf5bf855e64a5cc2306df861
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-12-01 23:40:23 -0600

    React.memo, index, Padre e Hijo y Documentacion

--- 20_10_21-05-Hook-App/src/components/07-tarea-memo/Padre.js
+++ 20_10_21-05-Hook-App/src/components/07-tarea-memo/Padre.js
@@ -4,25 +4,25 @@ import { useState } from 'react';
 
 import '../02-useEffect/effects.css';
 
-
 export const Padre = () => {
 
     const numeros = [2,4,6,8,10];
-    const [valor, setValor] = useState(0);
+    const [valor, setValor] = useState(0); //** Este es el useState y el parametro que mandamos es el initial state */
 
     // const incrementar = ( num ) => {
     //     setValor( valor + num )
     // }
 
-    //** Para solventar el problema, lo unico que se hizo fue ue creo la funcion incremenentar, que recibe y callback con el valor de numero, y el setValot le asigna el vlor al valor ue tiene, y el uiltimo tiene esa dependencia pero entre llaves */
-    const incrementar = useCallback( (num) => {
+    //** Para solventar el problema, lo unico que se hizo fue ue creo la funcion incremenentar, que recibe y callback con el valor de numero, y el setValot le asigna el valor al valor ue tiene, y el uiltimo tiene esa dependencia pero entre llaves */
+    const incrementar = useCallback( (num) => { //** Bien, aqui creo una funcion de flecha, con la constante incrementar, que modifica el valor del setValor, y incrementa segun lo que tenga en cada boton */
         setValor( v => v + num )
-    }, [ setValor ]);
+    }, [ setValor ]); //** Entre corchetes esta la dependencia para que no se vuelva arenderizar la funcion, esta la usamos mas para el memo */
 
 
 
     return (
         <div>
+            {/* Esto es lo que mostramos en el DOM */}
             <h1>Padre</h1>
             <p> Total: { valor } </p>
 
--- 20_10_21-05-Hook-App/src/index.js
+++ 20_10_21-05-Hook-App/src/index.js
@@ -11,13 +11,13 @@ import ReactDOM from 'react-dom';
 // import { Layout } from './components/05-useLayoutEffect/Layout';
 // import { Memorize } from './components/06-memos/Memorize';
 // import { MemoHook } from './components/06-memos/MemoHook';
-import { CallbackHook } from './components/06-memos/CallbackHook';
-// // import { Padre } from './components/07-tarea-memo/Padre';
+// import { CallbackHook } from './components/06-memos/CallbackHook';
+import { Padre } from './components/07-tarea-memo/Padre';
 // import { TodoApp } from './components/08-useReducer/TodoApp';
 // import { MainApp } from './components/09-useContext/MainApp';
 
 ReactDOM.render( //** Esto ya viene en el Index, solo le cambio el nombre con el cual voy a trabajar */
-    <CallbackHook/>,
+    <Padre/>,
   document.getElementById('root')
 );
 
