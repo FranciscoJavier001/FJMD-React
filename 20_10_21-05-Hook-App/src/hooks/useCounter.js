bb5314e4dcf407783494bb9ab82f0a8781808ad3
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2020-12-19 21:52:36 -0600

    Test useCounter y Documentacion

--- 20_10_21-05-Hook-App/src/hooks/useCounter.js
+++ 20_10_21-05-Hook-App/src/hooks/useCounter.js
@@ -25,7 +25,4 @@ export const useCounter = ( initialState = 10 ) => { //** Publica useCounter con
         decrement,
         reset
     };
-
 }
-
-
--- 20_10_21-05-Hook-App/src/tests/hooks/useCounter.test.js
+++ 20_10_21-05-Hook-App/src/tests/hooks/useCounter.test.js
@@ -1,3 +1,5 @@
+// No se puede hacer un snpshot porque no hay contenido HTML que evaluar, solo vamos a evaluar la logica
+
 import { renderHook, act } from '@testing-library/react-hooks';
 import { useCounter } from '../../hooks/useCounter';
 
@@ -6,10 +8,10 @@ describe('Pruebas en useCounter', () => {
 
     test('debe de retornar valores por defecto', () => {
         
-        const { result } = renderHook( () =>  useCounter() );
+        const { result } = renderHook( () =>  useCounter() ); //** Asi renderizamos el hook y desestructuramos el objeto, con el renderHook, que viene del testing-library de testing hooksy recibe un callback y el useCounter que se va a evaluar */
 
-        expect( result.current.counter ).toBe(10);
-        expect( typeof result.current.increment ).toBe('function');
+        expect( result.current.counter ).toBe(10); //** Aqui vemos la informacion que tiene el result, porque no estaba inicializado, y en el useCounter el initialState es de 10 */
+        expect( typeof result.current.increment ).toBe('function'); //** Asi comparamos si es una funcion */
         expect( typeof result.current.decrement ).toBe('function');
         expect( typeof result.current.reset ).toBe('function');
 
