56dc4b4db86a6b16965703943a289023b5051b93
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2021-01-03 17:36:59 -0600

    HomeScreen.test y Documentacion

--- 20_10_21-05-Hook-App/src/tests/components/09-useContext/HomeScreen.test.js
+++ 20_10_21-05-Hook-App/src/tests/components/09-useContext/HomeScreen.test.js
@@ -1,29 +1,25 @@
-import React from 'react';
+import React from 'react'; //** Importamos React porque lo vamos a ocupar */
 import { mount } from 'enzyme';
 import { HomeScreen } from '../../../components/09-useContext/HomeScreen';
 import { UserContext } from '../../../components/09-useContext/UserContext';
 
+describe('Pruebas en <HomeScreen />', () => { //** Pruebas en el nombre del componente, que no recibe ningun argumento */
 
-describe('Pruebas en <HomeScreen />', () => {
-
-    const user = {
+    const user = { //** Creamos una funcion que diga que usr va a ser igual a un nombre y un email */
         name: 'Francisco',
         email: 'francisco@gmail.com'
     }
 
-    const wrapper = mount(
-        <UserContext.Provider value={{
+    const wrapper = mount( //** Creamos nuestro wrapper, que va a ser igual al mount del homeScreen que se importa */
+        <UserContext.Provider value={{ //** Asi establecemos el contexto de la prueba, asi lo creamos importando el provider y lo cerramos y aqui proveemos el user y la informacion que vamos a proveer sera la informacion que se me ocurra en las pruebas, ponemos value con doble llave porque como estamos utilizando nuestro contexto estamos retornando un objeto que tiene el usuario va a ser  el que creamos arriba */
             user
         }}>
             <HomeScreen />  
         </UserContext.Provider>
     );
 
-    test('debe de mostrarse correctamente', () => {
+    test('debe de mostrarse correctamente', () => { //** La prueba basica */
 
-        expect( wrapper ).toMatchSnapshot();
-        
+        expect( wrapper ).toMatchSnapshot(); //** Hacemos que haga match con el snapshot  */
     })
-    
-    
 })
