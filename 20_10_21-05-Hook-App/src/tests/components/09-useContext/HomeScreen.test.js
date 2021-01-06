e3afbec792faac80878f4e7745ed86ad8edfd3a8
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2021-01-05 22:19:50 -0600

    HomeScreen.test y Documentacion

--- 20_10_21-05-Hook-App/src/tests/components/09-useContext/HomeScreen.test.js
+++ 20_10_21-05-Hook-App/src/tests/components/09-useContext/HomeScreen.test.js
@@ -7,3 +7,19 @@ describe('Pruebas en <HomeScreen />', () => { //** Pruebas en el nombre del comp
 
     const user = { //** Creamos una funcion que diga que usr va a ser igual a un nombre y un email */
         name: 'Francisco',
+        email: 'francisco@gmail.com'
+    }
+
+    const wrapper = mount( //** Creamos nuestro wrapper, que va a ser igual al mount del homeScreen que se importa */
+        <UserContext.Provider value={{ //** Asi establecemos el contexto de la prueba, asi lo creamos importando el provider y lo cerramos y aqui proveemos el user y la informacion que vamos a proveer sera la informacion que se me ocurra en las pruebas, ponemos value con doble llave porque como estamos utilizando nuestro contexto estamos retornando un objeto que tiene el usuario va a ser  el que creamos arriba */
+            user
+        }}>
+            <HomeScreen />  
+        </UserContext.Provider>
+    );
+
+    test('debe de mostrarse correctamente', () => { //** La prueba basica */
+
+        expect( wrapper ).toMatchSnapshot(); //** Hacemos que haga match con el snapshot  */
+    })
+})
