f5d5c1de59e2b917ed39133c051803900475fe0e
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2021-01-16 17:25:25 -0600

    LoginScreen y Documentacion

--- 21_01_12-07-Heroes-App/src/components/login/LoginScreen.js
+++ 21_01_12-07-Heroes-App/src/components/login/LoginScreen.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
-export const LoginScreen = () => {
+export const LoginScreen = ({ history }) => { //** Vamos a usar la desestructuracion y ahi vamos a tener el history */
+
+    const handleLogin = () => { //** Esta es una funcion de flecha, que no recibe nada */
+        history.push('/'); //** Asi le ponemos que va a hacer cuando toque el boton */
+        history.replace('/'); //** Investigar */
+    }
+
     return (
-        <div>
+        // Container es un contenedor, que se pone donde le digamos, si le aignamos un margen de arriba, osea es una linea que se queda solo en lo que vamos a poner
+        <div className="container mt-5">
             <h1>LoginScreen</h1>
+            <hr/>
+
+            <button
+                className="btn btn-primary"
+                onClick={ handleLogin } //** Este metodo hay que declararlo arriba, para que no nos de ninguna falla */
+            >
+                Login
+            </button>
         </div>
     )
 }
