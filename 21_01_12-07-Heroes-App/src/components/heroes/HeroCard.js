fdd71c1d8b3b814e70841c3228e53ef668eec144
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2021-01-20 15:09:51 -0600

    Targetas y Documentacion

---
+++ 21_01_12-07-Heroes-App/src/components/heroes/HeroCard.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+//** Esta es la informacion que esperamos recibir */
+export const HeroCard = ({
+  id,
+  superhero,
+  alter_ego,
+  first_appearance,
+  characters,
+}) => {
+  return (
+    // El Style lo puedo definir como un objeto para especifiar el el CSS que queramos pero no le podemos poner guines - porque eso va en contra de las propiedades de los objetos
+    <div className="card ms-3" style={{ maxWidth: 540 }}>
+      <div className="row no-gutters">
+        <div className="col-md-4">
+          <img
+            src={`./assets/heroes/${id}.jpg`}
+            className="card-img"
+            alt={superhero}
+          />
+        </div>
+        <div className="col-md-8">
+          <div className="card-body">
+            <h5 className="card-title"> {superhero}</h5>
+            <p className="card-text">{alter_ego}</p>
+            {alter_ego !== characters && ( //** Solamente si el alter_ego es diferente al caracter entonces que si se renderize eso */
+              <p className="card-text">{characters}</p>
+            )}
+            <p className="card-text">
+              <small className="text-muted"> {first_appearance} </small>
+            </p>
+            <Link to={ `./hero/${ id }`}>
+                Mas...
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- 21_01_12-07-Heroes-App/src/components/heroes/HeroList.js
+++ 21_01_12-07-Heroes-App/src/components/heroes/HeroList.js
@@ -1,23 +1,26 @@
 //** Iniciamos con rafc porque es un functional component */
 import React from 'react'
 import { getHeroesByPublisher } from '../../selectors/getHeroesByPublisher'
+import { HeroCard } from './HeroCard';
 
-export const HeroList = ({publisher}) => { //** Vamos a recibir el publisher en la funcion de flecha, este lo recibimos de data/heroes.js y publisher es quien publica la historieta, puede ser dc o marvel */
+export const HeroList = ({ publisher }) => { //** Vamos a recibir el publisher en la funcion de flecha, este lo recibimos de data/heroes.js y publisher es quien publica la historieta, puede ser dc o marvel */
 
     //** Aqui utilizamos nuestro selector y como no pensamos en mutarlo podemos crear la contante heroes y que sea getHeroesByPublisher y mandamos el publisher y ya tengo la data */
 
     const heroes = getHeroesByPublisher( publisher );
 
     return (
-        //** Voy a barrer cada uno de los elementos y mostrarlos en pantalla a ver si funciona, voy a cambiar esto por un unorder list, para barrer los heroes.map y parentesis para retornar un objeto y luego voy a colocar un listItem y el key eria igual al heroe.id y dentro del li voy a colocar solo el nombre del heroe.superheroe */
-        <ul>
+        //** Voy a barrer cada uno de los elementos y mostrarlos en pantalla a ver si funciona, voy a cambiar esto por un div, para barrer los heroes.map y parentesis para retornar un objeto y luego voy a colocar un HeroCard y el key eria igual al heroe.id y dentro del li voy a colocar solo el nombre del heroe.superheroe */
+        // Esto lo vamos a poner ahora como un div y le vamos a poner una clase de bootstrap
+        <div className="card">
             {
                 heroes.map( hero => (
-                    <li key={ hero.id }>
-                        { hero.superhero }
-                    </li>
+                    <HeroCard key={ hero.id }
+                        /* Como no se mostraba la informacion voy a mandar el operador spred para mostrar los datos, para extraer cada una de las propiedades del heroe  */
+                        { ...hero }
+                        />
                 ))
             } 
-        </ul>
+        </div>
     )
 }
