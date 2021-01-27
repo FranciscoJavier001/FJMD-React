63d515723e9b3adc6157fc0c69fb96ebfa8f3d74
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2021-01-26 19:10:16 -0600

    Optimizacion con useMemo y Documentacion

--- 21_01_12-07-Heroes-App/src/components/heroes/HeroList.js
+++ 21_01_12-07-Heroes-App/src/components/heroes/HeroList.js
@@ -1,13 +1,14 @@
 //** Iniciamos con rafc porque es un functional component */
-import React from 'react'
+import React, { useMemo } from 'react'
 import { getHeroesByPublisher } from '../../selectors/getHeroesByPublisher'
 import { HeroCard } from './HeroCard';
 
 export const HeroList = ({ publisher }) => { //** Vamos a recibir el publisher en la funcion de flecha, este lo recibimos de data/heroes.js y publisher es quien publica la historieta, puede ser dc o marvel */
 
     //** Aqui utilizamos nuestro selector y como no pensamos en mutarlo podemos crear la contante heroes y que sea getHeroesByPublisher y mandamos el publisher y ya tengo la data */
-
-    const heroes = getHeroesByPublisher( publisher );
+    
+    //** El useMemo es para que no este haciendo los cambios, porque ya memorizo el cache de la pagina, borramos esta linea que estaba abajo "const heroes = getHeroesByPublisher( publisher );", osea que no cambien las dependencias si no hay ningun cambio */
+    const heroes = useMemo(() => getHeroesByPublisher( publisher ), [publisher])
 
     return (
         //** Voy a barrer cada uno de los elementos y mostrarlos en pantalla a ver si funciona, voy a cambiar esto por un div, para barrer los heroes.map y parentesis para retornar un objeto y luego voy a colocar un HeroCard y el key eria igual al heroe.id y dentro del li voy a colocar solo el nombre del heroe.superheroe */
@@ -16,7 +17,8 @@ export const HeroList = ({ publisher }) => { //** Vamos a recibir el publisher e
         <div className="card-columns"> 
             {
                 heroes.map( hero => (
-                    <HeroCard key={ hero.id }
+                    <HeroCard 
+                    key={ hero.id }
                         /* Como no se mostraba la informacion voy a mandar el operador spred para mostrar los datos, para extraer cada una de las propiedades del heroe  */
                         { ...hero }
                         />
