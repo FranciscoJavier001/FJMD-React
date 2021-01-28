7c91e2b2b79c13766aead31c6a38f93ffe2b96f2
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2021-01-28 15:44:44 -0600

    SearchScreen, parsearlo y Documentacion

--- 21_01_12-07-Heroes-App/src/components/search/SearchScreen.js
+++ 21_01_12-07-Heroes-App/src/components/search/SearchScreen.js
@@ -1,21 +1,33 @@
 //** Este lo hacemos un FC */
-import React from 'react'
-import { heroes } from '../../data/heroes'
-import { useForm } from '../../hooks/useForm';
-import { HeroCard } from '../heroes/HeroCard'
+import React from 'react';
+import queryString from 'query-string'; //** Aqui voy a buscar el queryString */
 
-export const SearchScreen = () => {
+import { heroes } from '../../data/heroes';
+import { HeroCard } from '../heroes/HeroCard';
+import { useForm } from '../../hooks/useForm';
+import { useLocation } from 'react-router-dom';
+
+export const SearchScreen = ({ history }) => { //** Hisory va a estar adentro de un push */
+
+    //** Este es un hook para obtener la location facilmente */
+    const location = useLocation();
+    // console.log(location.search); //** Pasemos este objeto a lo que nos dice el queryString */
+    // console.log(queryString.parse( location.search )); //** Este lo voy a desestructurar por solo lo que ne interesa */
+    //** Asi queda */
+    const { q = '' } = queryString.parse( location.search ); //** Solamente me interesa el "q" del query */
+    console.log(q);
 
     const heroesFiltered = heroes;
     const [ formValues, handleInputChange ] = useForm({
-        searchText: '' //** El valor por defecto es un string vacio */
+        searchText: q //** Voy a pasar la "q" como un string vacio y se lo voy a establecer al valor inicial del formulario */
     });
 
     const { searchText } = formValues; //** Vamos a usar la desestructiracion del searchText */
 
     const handleSearch = (e) => { //** Tengo que recibir el evento */
-        e.preventDefault(); //** Porque es en el formulario */
-        console.log(searchText); //** Ya puedo imprimir lo que la persona escribe ahi */
+        e.preventDefault(); //** Porque es en el formulario, y este sirve para que no se recarge */
+        // console.log(searchText); //** Ya puedo imprimir lo que la persona escribe ahi */
+        history.push(`?q=${ searchText }`); //** Aqui en la URL ya cambio lo que se busco, osea ya salio lo que tenemos entre vacticks */
     }
 
     return (
