b025d091ad49c525194cb87699f44fd53370ed88
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2021-02-02 23:55:14 -0600

    Proteccion Rutas, Autentificacion y Documentacion

--- 21_01_12-07-Heroes-App/src/routers/AppRouter.js
+++ 21_01_12-07-Heroes-App/src/routers/AppRouter.js
@@ -1,15 +1,22 @@
 //** Este sistema de rutas principal solo es que este tiene el router, es la unica diferencia */
-import React from 'react'
+import React, { useContext } from 'react'
 import {
     BrowserRouter as Router,
     Switch,
     Route
   } from "react-router-dom";
 
+  import { AuthContext } from '../auth/AuthContext';
+  import { PrivateRoute } from './PrivateRoute';
+
 import { LoginScreen } from '../components/login/LoginScreen';
 import { DashboardRoutes } from './DashboardRoutes';
 
 export const AppRouter = () => { //** Esto es un FuntionalComponent comun y corriente */
+
+    // Para leer si esta autentificado
+    const { user } = useContext( AuthContext ) //** Primero voy a utilizar el user y en el segundo va a ser el AuthContext */
+
     return (
         <Router>
             <div>
@@ -18,7 +25,15 @@ export const AppRouter = () => { //** Esto es un FuntionalComponent comun y corr
                     {/* El loginScreen no tiene ningun Navbar porque no tiene ningun estilo */}
                     <Route exact path="/login" component={ LoginScreen }/>
                     {/* Esta es una ruta que no esta definida, vamos a renderizar el DashboardRoutes cuando estamos en el path="/", ahora aqui ya vemos el NavBar */}
-                    <Route path="/" component={ DashboardRoutes }/>
+                    {/* Este componente lo vamos a validar, para que el usuario este loggeado para ingresar a la app, y a continuacion vamos a proteger la ruta, entonces vamos a renderizar el componente de manera condicional */}
+
+                    {/* Esta la voy a proteger para proteger toda la App, cambio el Route que estaba al principio por el PrivateRoute */}
+                    <PrivateRoute 
+                    path="/" 
+                    component={ DashboardRoutes }
+                    //** Asi lo vamos a proteger con el isAutenticated */
+                    isAutenticated={ user.logged }
+                    />
                 </Switch>
             </div>
     </Router>
