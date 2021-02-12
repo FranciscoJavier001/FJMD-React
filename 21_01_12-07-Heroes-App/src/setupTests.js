3568cd8a7b47e6cf944d414b548aecf9228d353e
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2021-02-11 21:19:32 -0600

    Pruebas en authReducer y Documentacion

--- 21_01_12-07-Heroes-App/src/setupTests.js
+++ 21_01_12-07-Heroes-App/src/setupTests.js
@@ -1,9 +1,9 @@
 //** Hay que hacer la configuracion de enzyme y enzyme-to-json */
 
 import Enzyme from 'enzyme';
-import Adapter from 'enzyme-ada`ter-react-16';
+import Adapter from 'enzyme-adapter-react-16';
 import { createSerializer } from 'enzyme-to-json';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-expect.addSnapshoterializer(createSerializer({mode: 'deep'}));
+expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
