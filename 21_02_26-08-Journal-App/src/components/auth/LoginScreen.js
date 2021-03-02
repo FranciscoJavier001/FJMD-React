9448643b87d63d63cdb45045d7a14c7fd3c59a3e
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2021-03-02 17:16:56 -0600

    Modificaciones en el Auth

--- 21_02_26-08-Journal-App/src/components/auth/LoginScreen.js
+++ 21_02_26-08-Journal-App/src/components/auth/LoginScreen.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const LoginScreen = () => {
     return (
         <>
-            <h3>Login</h3>  {/* Es lo que esta escrito en la caja blanca definida en el auth__box-container que contiene a los 2 */}
+            <h3 className="auth__title">Login</h3>  {/* Es lo que esta escrito en la caja blanca definida en el auth__box-container que contiene a los 2 */}
 
             <form> {/* Este es la caja de un formulario que va a estar abajo del login */}
                 <input //** Este crea el primero */
@@ -25,7 +26,25 @@ export const LoginScreen = () => {
                 </button>
 
                 <hr />
-                Google
+                <div>
+                    <p>Login with Social Networks</p>
+                    <div 
+                        className="google-btn"
+                        >
+                        <div className="google-icon-wrapper">
+                            <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
+                        </div>
+                        <p className="btn-text">
+                            <b>Login with google</b>
+                        </p>
+                    </div>
+                </div>
+
+                <Link to="/auth/register" /* Este lo importamos desde react-router-dom con el link */
+                >
+                    Create new Account
+                </Link>
+
             </form>
         </>
     )
