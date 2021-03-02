d4924c1d0ce7cd6e50341a0d1e3ece0720571ad4
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2021-03-01 22:08:05 -0600

    Creacion de Estilos y Configuraciones SASS y Doc.

--- 21_02_26-08-Journal-App/src/index.js
+++ 21_02_26-08-Journal-App/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { JournalApp } from './JournalApp';
+//** Los cambios que estoy implementando en styles.scss se ven reflejados ya, porque ya importe lo que necesito */
 import './styles/styles.scss'
 
 ReactDOM.render(
