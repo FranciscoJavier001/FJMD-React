e21a7279df8498411f2e5be0f895185e1a40161d
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2021-03-11 10:54:54 -0600

    Sidebar y estilos

--- 21_02_26-08-Journal-App/src/components/journal/Sidebar.js
+++ 21_02_26-08-Journal-App/src/components/journal/Sidebar.js
@@ -5,9 +5,21 @@ export const Sidebar = () => {
         <aside className="journal__sidebar"> {/* El aside indica que esto esta en un costado y le asignamos una clase para ponerla con scss */}
             
             <div className="journal__sidebar-navbar">
-                <h3>
-                    <span>Francisco</span>
+                <h3 className="mt-5"> {/* Hicimos una separacion hacia abajo para que no se viera tan arriba */}
+                    <i className="far fa-moon"></i>
+                    <span> Francisco</span>
                 </h3>
+
+                <button className="btn">
+                    Logout
+                </button>
+            </div>
+
+            <div className="journal__new-entry">
+                <i className="far fa-calendar-plus fa-5x"></i> {/* Con esto creo un calendario, es de font awesome */}
+                <p className="mt-5"> {/* Para hacer una separacion del New Entry hacia arriba */}
+                    New Entry
+                </p>
             </div>
         </aside>
     )
