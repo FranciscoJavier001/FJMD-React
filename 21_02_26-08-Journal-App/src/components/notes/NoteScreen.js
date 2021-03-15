b722c8585cd8a4a0a28e9d6df346bea5a0a46e9b
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2021-03-15 17:51:38 -0600

    Estructura HomeScreen y Estilos

--- 21_02_26-08-Journal-App/src/components/notes/NoteScreen.js
+++ 21_02_26-08-Journal-App/src/components/notes/NoteScreen.js
@@ -6,6 +6,29 @@ export const NoteScreen = () => {
         <div className="notes__main-content">
 
             <NotesAppBar /> {/* Igual no va a recibir ningun argumento */}
+
+            <div className="notes__content"> {/* Este va a ser el agrupador de mi formulario */}
+
+                <input
+                    type="text"
+                    placeholder="Some Awesome Title"
+                    className="notes__title-input"
+                    autoComplete="off"
+                />
+
+                <textarea
+                    placeholder="What Happend Today"
+                    className="notes__textarea"
+                ></textarea>
+
+                <div className="notes__image">
+                    <img
+                        src="https://image.freepik.com/foto-gratis/silueta-mujer-joven-meditando-practicando-yoga-playa-al-atardecer_35708-291.jpg"
+                        alt="siluet"
+                    />
+                </div>
+
+            </div>
             
         </div>
     )
