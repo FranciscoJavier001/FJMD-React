26b09dca7ce2ee4cb2f6a91f6528c6c17179de87
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2021-03-16 22:51:26 -0600

    Configuracion de Rediux

--- 21_02_26-08-Journal-App/src/store/store.js
+++ 21_02_26-08-Journal-App/src/store/store.js
@@ -5,5 +5,8 @@ const reducers = combineReducers({ //** Abro y cierro llaves para especificar un
     auth: authReducer//** Este va a tener la estructira que va a tener nuestro store en general, voy a tener una propiedad llamada auth: que va a ser manejada por mi authReducer */
 }) 
 
-export const store = createStore(reducers); //** Este recibe un reducer y solo recibe 1 */
-//** Hay que decirle que hay una fuente unica de la verdad y hay que ponerlo en el punto mas alto, pero no en el index jajaja, sino en el JournalApp */
+export const store = createStore(
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    ); //** Este recibe un reducer y solo recibe 1 */
+//** Hay que decirle que hay una fuente unica de la verdad y hay que ponerlo en el punto mas alto, pero no en el index jajaja, sino en el JournalApp, y el segundo argumento que le pusimos fue para que se habilitara el store en la pestaña */
