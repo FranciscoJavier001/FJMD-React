8510fc31a8b175d17bbedc2a223f4fc91e6532d1
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2021-05-12 14:00:43 -0500

    Keep login state

--- 21_02_26-08-Journal-App/src/routers/AppRouter.js
+++ 21_02_26-08-Journal-App/src/routers/AppRouter.js
@@ -1,6 +1,6 @@
 //** Atencion, mira estas son las rutas del AppRouter, como lo vemos no son tan complicadas, solo se necesita entender los parametros principales de hacia donde va la aplicacion */
 
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import {
     BrowserRouter as Router,
     Switch,
@@ -8,22 +8,45 @@ import {
     Redirect
 } from "react-router-dom"
 
+import { useDispatch } from 'react-redux'
+
 import { firebase } from '../firebase/firebase-config'
 
 import { AuthRouter } from './AuthRouter'
 import { JournalScreen } from '../components/journal/JournalScreen'
+import { login } from '../actions/auth'
 
 export const AppRouter = () => {
+
+    const dispatch = useDispatch();
+
+    //** Para evaluar si esta autentificado para esto voy a crearme un estado local en el useState */
+    const [ checking, setChecking ] = useState(true); //** Que tenga la variable de checking como que esta revisando el estado de firebase, pero mientras esto sea true no voy a mostrar nada mas de la aplicacion */
+    const [ isLoggedIn, setIsLoggedIn ] = useState(false)
 
     //** Este efecto lo vamos a utilizar, para que aunque el usuario salga de la pagina, su sesion se mantenga */
     useEffect(() => { //** Por el momento aqui no voy a tener ninguna dependencia */
         //** Del archio de firebase hay un archivo importado de auth que lo vamos a utilizar */
         firebase.auth().onAuthStateChanged( (user) => {
 
-            console.log(user);
-            
+            if (user?.uid) { //** Esto quiere decir que si estoy autentificado, el "?" evalua si el objeto user tiene algo */
+                dispatch( login( user.uid, user.displayName ) ) //** Ahora puedo hacer el dispatch de cualquier accion que se me venga en gana, ahora voy a hacer el dispatch mandando como argumento el user.uid y el user.displayName, de aqui puedo extraer todo lo que necesite */
+                setIsLoggedIn( true ) //** Esto quiere decir qe esta loggeado de manera correcta */
+            } else {
+                setIsLoggedIn( false ) //** Pues en caso contrario pues no esta loggeado */
+            }
+
+            //** El setChecking voy a mostrarlo aqui, cuando ya tengo una respuestade firebase */
+            setChecking(false) //** De esta manera ya tengo la respuesta, osea ya se si tengo el usuario o no lo tengo */
+
         })
-    }, [])
+    }, [ dispatch, setChecking, setIsLoggedIn ]) //** Esto solo se ejecuta una vez y esto es un observable que va a estar al pendiente de los cambios en la autentificacion del usuario, si cambia la autentificacion esto va a cambiar, es decir el callback se va a llamar */
+
+    if ( checking ){
+        return ( //** Si esta en true voy a hacer el return de un nuevo objeto */
+            <h1>Espere...</h1>
+        )
+    }
 
     return (
         <Router>
