1ef4389b62f64d3718f5b2f5176c726c8589bcf0
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2021-05-29 20:12:02 -0500

    Create New Note

--- 21_02_26-08-Journal-App/src/actions/notes.js
+++ 21_02_26-08-Journal-App/src/actions/notes.js
@@ -1,4 +1,5 @@
 import { db } from "../firebase/firebase-config";
+import { types } from '../types/types'
 
 //** Voy a exportar un nuevo componente */
 export const startNewNote = () => { //** Esta es una tarea asyncrona, asi que voy a poner el return con un callback que va a ocupar el dispatch, y lo voy a llamar en el dispatch del Sidebar.js en el handleAddNew */
@@ -17,6 +18,18 @@ export const startNewNote = () => { //** Esta es una tarea asyncrona, asi que vo
         //** Para hacer lo del getTime, debo de conseguir la referencia a la base de datos, esta ya la tengo en firebase & firebase-config.js */
         const doc = await db.collection(`${ uid }/journal/notes`).add( newNote ) //** Voy a hacer la referencia al documento y esto viene de db que lo explique arriba, esto lo voy a poner en la coleccion donde quiero guardar esto, voy a ocupar el uid del usuario , luego el journal que va a ser donde lo estoy guardando, luego notes, porque quiero que ahi este y ahi ya puedo hacer el .add y ahi mando el newNote, el add regresa una promesa que retorna el document.reference y como esto es una promesa en el return del startNewNote lo voy a convertir a async y aqui hacer el await, osea espera que se haga la insercion y el doc va a dar referencia al documento */
 
-        console.log(doc);
+        //** Aqui voy a mandar llamar la accion que defini abajo */
+        dispatch( activeNote( doc.id, newNote ) ) //** Esta pide el id de la nota y este lo tengo en el doc.id, y luego viene la nota que seria el newNote que lo acabo de crear */
     }
 }
+
+//** Lo ultimo despues de todo lo que hemos echo es manejar esta accion el noteReducer */
+//** Voy a crear una nueva accion que sea syncrona porque ya voy a tener la informacion voy a mandar algo al reducer  */
+export const activeNote = ( id, note ) => ({ //** Este va a recibir el id y la nota */
+    //** Este objeto va a tener los types */
+    type: types.notesActive,
+    payload: {
+        id,
+        ...note
+    }
+})
--- 21_02_26-08-Journal-App/src/components/notes/notesReducer.js
+++ 21_02_26-08-Journal-App/src/components/notes/notesReducer.js
@@ -1,5 +1,7 @@
 //** Aqui voy a poner como quiero que este el State */
 
+import { types } from "../../types/types";
+
 //** Va a tener notes como un arreglo, ese es el primero y el segundo va a ser el active, que puede ser null o va a poder tener el objeto de la nota activa, si tenemos null, no hay ninguna nota seleccionada y si esta activa, va a tener el id, que va a ser un id unico de firebase y el title como un string y el body que tambien va a ser un string , la url de la imagen y una fecha  */
 
 //** El reducer va a ser una funcion pura */
@@ -11,6 +13,15 @@ const initialState = { //** Este va a ser el estado inicial del reducer */
 export const notesReducer = ( state = initialState, action ) => { //** Recuerda primero ponemos lo que recibe, y el state va a ser igual al initialState */
 
     switch (action.type) { //** Aqui voy a empezar a definir muchos tipos */
+
+        //** Despues de hacer lo de la nueva nota con el dispatch en el notes, voy a poner el tipo aqio */
+        case types.notesActive:
+            return { //** Aqui lo que voy a hacer va a ser regresar un nuevo estado, osea la clonacion del estado anterior, para siempre regresar un nuevo estado, no mutar el anterior */
+                ...state,
+                active: { //** Voy a regresar la nota activa que va a ser un objeto del action.payload */
+                    ...action.payload
+                }
+            }
 
         default:
             return state; //** Solamente voy a hacer el return del state */
