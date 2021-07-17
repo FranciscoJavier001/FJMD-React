d13faa87e50e61d331385f98ba4b7425977d5996
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2021-07-16 21:07:10 -0500

    TuneUp in fileUpload

--- 21_02_26-08-Journal-App/src/tests/helpers/fileUpload.test.js
+++ 21_02_26-08-Journal-App/src/tests/helpers/fileUpload.test.js
@@ -10,28 +10,28 @@ cloudinary.config({ //** Esto es de la documentacion de Cloudinary */
 describe('Pruebas en fileUpload', () => {
 
     //** Si borra el archivo, pero me da falla al subirlo porque checa que ya no esta */
-    test('debe de cargar un archivo y retornar el URL', async() => { //** Aqui puedo hacer el async, y le digo que aguante hasta que llame al done */
+    test('debe de cargar un archivo y retornar el URL', async() => { //** Aqui puedo hacer el async, y le digo que aguante hasta que llame al done, elimine el done como en la parte de abajo */
         
-        const resp = await fetch('https://sites.google.com/a/netcmmi.com/share/_/rsrc/1473734124982/img/png/s/star-e01.png') //** fetch me regresa una promesa y el await es como aguantala y copiamos el url de la imagen */
+        const resp = await fetch('https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png') //** fetch me regresa una promesa y el await es como aguantala y copiamos el url de la imagen */
         const blob = await resp.blob()
 
-        const file = new File([blob], 'foto.google')
+        const file = new File([blob], 'foto.png')
         const url = await fileUpload( file )
 
         expect( typeof url ).toBe('string')
 
         //** Borrar imagen por id */
         const segments = url.split('/')
-        const imageId = segments[ segments.length -1 ].replace('.jpg', '')
+        const imageId = segments[ segments.length -1 ].replace('.png', '')
 
         cloudinary.v2.api.delete_resources(imageId, {}, ()=> { //** Para borrar */
-        //     done(); //** Prueba fallo, asi que mande alv el done del async de arriba para evitar el error, y me funciona */
+            // done(); //** Prueba fallo, asi que mande alv el done del async de arriba para evitar el error, y me funciona */
         });
     })
 
     test('debe de retornar un error', async() => { //** Aqui puedo hacer el async */
 
-        const file = new File([], 'foto.google')
+        const file = new File([], 'foto.png')
         const url = await fileUpload( file )
 
         expect( url ).toBe( null )
