32aa2eb21183cd08dbdbeb944fdce58f56a40c30
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2021-08-05 23:54:06 -0500

    Descanso - A la Playa!!!

--- 21_08__04-09-Calendar-App/src/index.js
+++ 21_08__04-09-Calendar-App/src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    {/* A la Playa!!! */}
   </React.StrictMode>,
   document.getElementById('root')
 );
