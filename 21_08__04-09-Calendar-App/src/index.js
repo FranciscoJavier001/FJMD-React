b530b187fe5a50cc47979e237a996ca5eae06506
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2021-08-10 21:48:28 -0500

    Init Project

--- 21_08__04-09-Calendar-App/src/index.js
+++ 21_08__04-09-Calendar-App/src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    {/* A la Playa!!! */}
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
