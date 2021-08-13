310d439db8f86e0540806cd4ccf31ee75c0d6320
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2021-08-12 23:49:11 -0500

    Install CDN's

--- 21_08_04-09-Calendar-App/src/index.js
+++ 21_08_04-09-Calendar-App/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { CalendarApp } from './CalendarApp';
 
 ReactDOM.render(
-    <App />,
+    <CalendarApp />,
   document.getElementById('root')
 );
