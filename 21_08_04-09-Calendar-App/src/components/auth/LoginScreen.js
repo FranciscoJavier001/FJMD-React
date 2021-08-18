0f2e7cabb43e528dccba09984dc2f28cb1ee00b6
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2021-08-17 21:31:58 -0500

    Updating LoginScreen Info

--- 21_08_04-09-Calendar-App/src/components/auth/LoginScreen.js
+++ 21_08_04-09-Calendar-App/src/components/auth/LoginScreen.js
@@ -1,31 +1,31 @@
 import React from 'react';
-import './login.css';
+import './login.css'; //** Esto es porque importamos info desde nuestro css */
 
-export const LoginScreen = () => {
+export const LoginScreen = () => { //** Esto es porque lo exportamos al archivo de rutas */
     return (
-        <div className="container login-container">
-            <div className="row">
-                <div className="col-md-6 login-form-1">
-                    <h3>Ingreso</h3>
+        <div className="container login-container"> {/* El Container es el contenedor dentro de la pantalla, para que no abarque todo el espacio de la pantalla, y el login-container es la clase que establecimos en el CSS de el margen superior que tiene en la pantalla */}
+            <div className="row"> {/* Esto de Row quiere decir que solo va en una fila */}
+                <div className="col-md-6 login-form-1"> {/* El col-md-6 nos dice lo que va a medir el formulario de ingreso y lo demas es la clase que ya definimos */}
+                    <h3>Ingreso</h3> 
                     <form>
-                        <div className="form-group">
-                            <input 
+                        <div className="form-group"> {/* Es un formulario en grupo, osea aqui el correo, pero solo va un espacio */}
+                            <input //** Aqui va la info que va por el formulario y luego lo cerramos */
                                 type="text"
                                 className="form-control"
                                 placeholder="Correo"
                             />
                         </div>
-                        <div className="form-group">
+                        <div className="form-group"> {/* Creamos otro formulario pero con lo del password */}
                             <input
                                 type="password"
                                 className="form-control"
                                 placeholder="Contraseña"
                             />
                         </div>
-                        <div className="form-group">
+                        <div className="form-group"> {/* Igual creamos otro formulario, osea hay que crear uno de cada uno independiente */}
                             <input 
                                 type="submit"
-                                className="btnSubmit col-md-6"
+                                className="btnSubmit col-md-6" //** Lo primero es una clase de bootstrap, lo segundo es la distancia que manera en el form-roup */
                                 value="Login" 
                             />
                         </div>
