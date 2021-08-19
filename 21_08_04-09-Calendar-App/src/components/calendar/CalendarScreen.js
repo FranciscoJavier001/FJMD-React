7ea52ed02cc8ff7b7e1ac4275f7fcaae80f7044f
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2021-08-19 17:23:44 -0500

    Imports, Styles, in Calendar-App

--- 21_08_04-09-Calendar-App/src/components/calendar/CalendarScreen.js
+++ 21_08_04-09-Calendar-App/src/components/calendar/CalendarScreen.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import { Calendar, momentLocalizer } from 'react-big-calendar' //** Hice estas dos importaciones */
+import moment from 'moment' //** De las que dije */
+
 import { Navbar } from '../ui/Navbar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+const localizer = momentLocalizer(moment) //** Esto lo copie, y simplemente lo puse despues de las importaciones */
+
+//** Los eventos pueden lucir como nosotros queramos, pero debe tener ciertas caracteristicas, para que este big-calendar lo pueda interpretar */
+const events = { //** Este va a ser un arreglo y dentro va a tener objetos */
+    title: 'Cumpleaños del jeje', //** Esto va a ser el titulo que se va a mostrar */
+    start: moment().toDate(), //** Esto es cuando inicia, deberia hacerlo con moment, pero lo hace con librerias de JS, el new Data() es para saber el momento exacto, pero lo vamos a hacer con moment */
+    end: moment().add( 2, 'hours' ).toDate(), //** Con esto lo finalizamos a las 2 horas despues y es algo de moment chido */
+    bgcolor: '#fafafa' //** Esto solo es un background de la propiedad */
+}   
 
 export const CalendarScreen = () => {
     return (
-        <div>
+        <div className="calendar-screen"> {/* Aqui le voy a poner un nuevo estilo por eso hago una nueva clase */}
             <Navbar />
+
+            <Calendar
+                localizer={ localizer }
+                events={[ events ]} //** Esos eventos son los que voy a mandar aqui, los que puse en la linea 10 */
+                startAccessor="start"
+                endAccessor="end"
+            />
         </div>
     )
 }
--- 21_08_04-09-Calendar-App/src/components/ui/Navbar.js
+++ 21_08_04-09-Calendar-App/src/components/ui/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Navbar = () => {
     return (
-        <div className="navbar navbar-dark bg-dark">
+        <div className="navbar navbar-dark bg-dark mb-4">
             <span className="navbar-brand">
                 Juan!!
             </span>
