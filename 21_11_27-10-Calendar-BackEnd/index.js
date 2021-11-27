f8100731b5d1d7989e4e5ad244f18d75789c0fda
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2021-11-27 01:33:30 -0600

    Express Instalation

--- 21_11_27-10-Calendar-BackEnd/index.js
+++ 21_11_27-10-Calendar-BackEnd/index.js
@@ -1,2 +1,19 @@
 
-console.log('Hola Mundo!!');
+//** Configuracion basica de express */
+const express = require('express')
+
+// Crear el servidor de express
+const app = express();
+
+// Rutas
+app.get('/', (req, res) => {
+
+    res.json({
+        ok: true
+    })
+})
+
+// Escuchar peticiones
+app.listen( 4000, () => {
+    console.log(`Servidor corriendo en puerto ${ 4000 }`);
+})
