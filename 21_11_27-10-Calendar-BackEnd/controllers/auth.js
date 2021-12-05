502c52b38794322480ead1042dfa8cb753871a5b
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2021-12-04 20:44:45 -0600

    Mongo Settings - Create User

--- 21_11_27-10-Calendar-BackEnd/controllers/auth.js
+++ 21_11_27-10-Calendar-BackEnd/controllers/auth.js
@@ -1,17 +1,27 @@
 const { response } = require('express')
+const Usuario = require('../models/Usuario')
 
-const crearUsuario = (req, res = response ) => { 
+const crearUsuario = async(req, res = response ) => { 
 
-    //** Aqui voy a desestructurar lo que es el body y voy a extraer las 3 cosas que ocuparia */
-    const { name, email, password } = req.body;
+    //** Aqui voy a desestructurar lo que es el body y voy a extraer las 3 cosas que ocuparia, esto es lo que viene el el body desestructurado */
+    // const { name, email, password } = req.body;
+    try {
+        const usuario = new Usuario( req.body )
 
-    res.status(201).json({
-        ok: true,
-        msg: 'registro',
-        name,
-        email,
-        password
-    })
+        await usuario.save()
+
+        res.status(201).json({
+            ok: true,
+            msg: 'registro',
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        })
+    }
 }
 
 const loginUsuario = (req, res = response ) => {
--- 21_11_27-10-Calendar-BackEnd/models/Usuario.js
+++ 21_11_27-10-Calendar-BackEnd/models/Usuario.js
@@ -1,1 +1,19 @@
-const mongoose = require('mongoose')
+const { Schema, model } = require('mongoose')
+
+const UsuarioSchema = Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true
+    }
+})
+
+module.exports = model('Usuario', UsuarioSchema )
