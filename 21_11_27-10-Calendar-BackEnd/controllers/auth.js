6e759b3c384cbd21032c687a14563999dcbbc2b6
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2021-12-08 03:42:13 -0600

    Login Usuario

--- 21_11_27-10-Calendar-BackEnd/controllers/auth.js
+++ 21_11_27-10-Calendar-BackEnd/controllers/auth.js
@@ -41,16 +41,46 @@ const crearUsuario = async(req, res = response ) => {
     }
 }
 
-const loginUsuario = (req, res = response ) => {
+const loginUsuario = async(req, res = response ) => {
 
     const { email, password } = req.body;
 
-    res.json({
-        ok: true,
-        msg: 'login',
-        email,
-        password
-    })
+    try {
+        
+        const usuario = await Usuario.findOne({ email })
+        
+        if ( !usuario ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El usuario no existe con ese email'
+            })
+        }
+
+        // Confirmar los passwords
+        const validPassword = bcrypt.compareSync( password, usuario.password )
+
+        if ( !validPassword ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Password incorrecto'
+            })
+        }
+
+        // Generar nuestro JWT
+
+        res.json({
+            ok: true,
+            uid: usuario.id, 
+            name: usuario.name
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        })
+    }
 }
 
 const revalidarToken = (req, res = response ) => {
