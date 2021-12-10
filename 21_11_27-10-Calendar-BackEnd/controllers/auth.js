c47d7321078e5455de412f0e1c73c2dd8c9c7412
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2021-12-09 22:17:05 -0600

    Validaciones y Documentacion

--- 21_11_27-10-Calendar-BackEnd/controllers/auth.js
+++ 21_11_27-10-Calendar-BackEnd/controllers/auth.js
@@ -4,11 +4,10 @@ const Usuario = require('../models/Usuario')
 
 const crearUsuario = async(req, res = response ) => { //** Aqui voy a crear el Usuario */
 
-    //** Aqui voy a desestructurar lo que es el body y voy a extraer las 3 cosas que ocuparia, esto es lo que viene el el body desestructurado */
     const { email, password } = req.body; //** Esto es lo que va a necesitar el Campo, para crear el Usuario */
 
     try {
-        let usuario = await Usuario.findOne({ email })
+        let usuario = await Usuario.findOne({ email }) //** Aqui voy a buscar un usuario mediante el email */
         
         if ( usuario ) {
             return res.status(400).json({
@@ -41,7 +40,7 @@ const crearUsuario = async(req, res = response ) => { //** Aqui voy a crear el U
     }
 }
 
-const loginUsuario = async(req, res = response ) => { //** Vamos a trabajar con esta funcion 211208-3:46a, es una funcion asincrona por el await, que osea va a esperar una respuesta */
+const loginUsuario = async(req, res = response ) => { //** Vamos a trabajar con esta funcion 211208-3:46a, asincrona por el await espera/respuesta */
 
     const { email, password } = req.body; //** Esta la hicimos una constante, porque no va a cambiar */
 
@@ -49,37 +48,38 @@ const loginUsuario = async(req, res = response ) => { //** Vamos a trabajar con 
         
         const usuario = await Usuario.findOne({ email }) //** Esta es la instruccion */
         
-        if ( !usuario ) { //** Si el Usuario, no existe, mandar esto, osea asi lo retornamos, el status 400, ok, el estado que esta en falla, y el msg porque esa es la falla que arroja */
-            return res.status(400).json({
-                ok: false,
-                msg: 'El usuario no existe con ese email'
+        if ( !usuario ) { //** Si el Usuario, no existe, mandar esto, osea asi lo retornamos */
+            return res.status(400).json({ //** Si el error es 400, es porque el usuario ya existe */
+                ok: true, //** Este simplemente es porque asi lo mandamos, si dejo el cursor en el ok me sale el tipo de variable que recibe */
+                msg: 'El usuario no existe con ese email' //** Este es el mensaje que mandamos */
             })
         }
 
         // Confirmar los passwords
-        const validPassword = bcrypt.compareSync( password, usuario.password ) //** Aqui vamos a validar los passwords, el que estamos mandando y el que tenemos en la base de datos, pero el que ya esta encriptado */
+        const validPassword = bcrypt.compareSync( password, usuario.password ) //** Validarmos los passwords de ambos campos, en db y mandamos */
 
-        if ( !validPassword ) { //** Si es diferente al password, entonces mandame el error 400, con su estado que es el ok y el msg que es la falla */
-            return res.status(400).json({
-                ok: false,
-                msg: 'Password incorrecto'
+        if ( !validPassword ) { //** Si es diferente al password */
+            return res.status(400).json({ //** Me retorna una falla, por eso respuesta en el 400 */
+                ok: false, //** El tipo de dato que me retorna es false porque esta mal una validacion */
+                msg: 'Password incorrecto' //** Mesaje de que el pass esta mal */
             })
         }
 
         // Generar nuestro JWT
 
-        res.json({ //** Pero si es usuario correcto, te mando el ok en true, el uid que es igual usuario.id y el nombre, pero, el primero es pura mamada, porque es como yo los quiera llamar */
-            ok: true,
-            uid: usuario.id, 
-            name: usuario.name,
-            // password: usuario.password
+        res.json({ //** Si la validacion del email y pass es correcta entonces esto me retorna */
+            ok: true, //** El estado es true */
+            uid: usuario.id, //** Mandamos el uid:/id del usuario que son puros numeros */
+            name: usuario.name, //** Mandamos el nombre del usuario */
+            // password: usuario.password //** Hasta podriamos mandar la contraseña */
         })
 
-    } catch (error) { //** Si la falla ya no es del usuario, sino nuestra mandamos la falla que la consola revento, osea como si no pudieramos entrar a la base de datos */
-        console.log(error);
-        res.status(500).json({
-            ok: false,
-            msg: 'Por favor hable con el administrador'
+        //** Este puede ser, por fallas mas alla, como por ejemplo que mandemos el ok con otra cosa que no sea un booleano */
+    } catch (error) { //** Estas pueden ser fallas en la respuesta del formulario, algun campo mal identificado */
+        console.log(error); //** Mandamos el error en consola, pero esto es en la pagina */
+        res.status(500).json({ //** Es diferente el campo */
+            ok: false, //** Por ser booleano y mandamos la falla */
+            msg: 'Por favor hable con el administrador' //** El mensaje que va a retornar */
         })
     }
 }
