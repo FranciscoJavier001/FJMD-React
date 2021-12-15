e263d26d0e441351ca7d74d729faec44ed2e7c79
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2021-12-14 23:21:35 -0600

    Configuracion CORS

--- 21_11_27-10-Calendar-BackEnd/index.js
+++ 21_11_27-10-Calendar-BackEnd/index.js
@@ -1,6 +1,7 @@
 //** Configuracion basica de express */
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors')
 const { dbConnection } = require('./database/config');
 
 // Crear el servidor de express
@@ -8,6 +9,9 @@ const app = express();
 
 // Base de datos
 dbConnection()
+
+// CORS
+app.use(cors())
 
 // Directorio Publico
 app.use( express.static('public') ) //** Asi le hicimos para mostrar la pagina en el localhost:4000 */
