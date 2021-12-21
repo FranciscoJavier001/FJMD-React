02946ba3062c45127810e82e3d64461123ff1f38
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2021-12-21 17:51:53 -0600

    CrearEvento, Postman, Moongo

--- 21_11_27-10-Calendar-BackEnd/controllers/events.js
+++ 21_11_27-10-Calendar-BackEnd/controllers/events.js
@@ -1,4 +1,5 @@
 const { response } = require('express') //** Importamos la response, para usarla abajo */
+const Evento = require('../models/Evento') //** La referencia al modelo para guardar evento models>Evento, tiene necesidades */
 
 //** Recuerda que van por los headers, para no olvidarlo en postman */
 const getEventos = (req, res = response ) => { //** Esta la importamos en routes>events */
@@ -7,14 +8,29 @@ const getEventos = (req, res = response ) => { //** Esta la importamos en routes
         msg: 'getEventos' //** Mensaje */
     })
 }
-const crearEvento = (req, res = response ) => { //** Esta la importamos en routes>events */
+const crearEvento = async (req, res = response ) => { //** Esta la importamos en routes>events, tarea asincrona */
     //** Postman>Even-CrearEvento (Actualiza Token en Auth-Login y pasalo por Header en CrearEvento) */
-    console.log( req.body ); //** Con esto me aseguro que tengo el evento */
+    // console.log( req.body ); //** Con esto me aseguro que tengo el evento */
+    const evento = new Evento( req.body ); //** evento es nuevo, lo importamos y va a recibir los parametros definidos de Evento */
 
-   res.json({ //** Esto me retorna el arreglo al hacer la peticion en postman routes>events */
-        ok: true, //** El estado */
-        msg: 'crearEventos' //** Mensaje */
-    })
+    try {
+
+        evento.user = req.uid; //** Necesitamos el id del usuario y asi lo extraemos, viene en models>Evento en user */
+        
+        const eventoGuardado = await evento.save() //** Asi lo guardamos, queda guardado en la variable eventoGuardado */
+
+        res.json({ //** Esto me retorna el arreglo al hacer la peticion en postman routes>events */
+            ok: true, //** El estado */
+            evento: eventoGuardado  //** Lo vamos a mostrar completo */
+        })
+
+    } catch (error) { //** En caso de falla asi se configura */
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador...'
+        })
+    }
 }
 const actualizarEvento = (req, res = response ) => { //** Esta la importamos en routes>events */
     res.json({ //** Esto me retorna el arreglo al hacer la peticion en postman */
--- 21_11_27-10-Calendar-BackEnd/models/Evento.js
+++ 21_11_27-10-Calendar-BackEnd/models/Evento.js
@@ -1,27 +1,37 @@
 //** Este es un modelo para realizar posteos, actualizaciones, para info etc..  */
 const { Schema, model } = require('mongoose') //**  */
 
+//** Estos son parametros que necesita el evento para crearse, lo defini en crearEvento en controllers>events confirmo Postman */
 const EventoSchema = Schema({
 
     title: {
         type: String,
-        require: true
+        required: true
     },
     notes: {
         type: String,
     },
     start: {
         type: Date,
-        require: true
+        required: true
     },
     end: {
         type: Date,
-        require: true
+        required: true
     },
     user: {
         type: Schema.Types.ObjectId, //** Esto le dice a Moongose que va a ser una referencia */
-        ref: 'Usuario' //** La referencia de arriba va a ser la del usuario (nombre del otro Schema) */
+        ref: 'Usuario', //** La referencia de arriba va a ser la del usuario (nombre del otro Schema) */
+        required: true //** Voy a pedir que el usuario sea requerido */
     }
+})
+
+//** Asi definimos como queremos que se muestre en Postman */
+EventoSchema.method('toJSON', function() {
+    //** La referencia al objeto que se esta serializa, voy a extraer cosas, y lo demas va a estar en object*/
+    const { __v, _id, ...object } = this.toObject()
+    object.id = _id //** Ahora quiero que se llame id y lo segundo es lo que es */
+    return object //** Voy a retornr object como nuevo */
 })
 
 module.exports = model('Evento', EventoSchema ) //** Va a estar en controllers>events */
