947eb228e1581277e32a43fa70160c9f14a5db09
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2021-12-23 14:31:26 -0600

    Problema con Delete

--- 21_11_27-10-Calendar-BackEnd/controllers/events.js
+++ 21_11_27-10-Calendar-BackEnd/controllers/events.js
@@ -83,11 +83,48 @@ const actualizarEvento = async(req, res = response ) => { //** Esta la importamo
     }
 }
 
-const eliminarEvento = (req, res = response ) => { //** Esta la importamos en routes>events */
-    res.json({ //** Esto me retorna el arreglo al hacer la peticion en postman */
-        ok: true, //** El estado */
-        msg: 'eliminarEvento'
-    })
+// const eliminarEvento = (req, res = response ) => { //** Esta la importamos en routes>events */
+    // res.json({ //** Esto me retorna el arreglo al hacer la peticion en postman */
+    //     ok: true, //** El estado */
+    //     msg: 'eliminarEvento'
+    // })
+
+const eliminarEvento = async(req, res = response ) => { 
+    const eventoId = req.params.id
+    const uid = req.uid
+
+    try {
+        
+        const evento = await Evento.findById( eventoId )
+
+        if ( !evento ) {
+            res.status(404).json({
+                ok: false,
+                msg: 'Evento no existe por ese id'
+            })
+        }
+
+        if ( evento.user.toString() !== uid ) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegio para eliminar este evento'
+            })
+        }
+
+        const eventoEliminado = await Evento.findByIdAndDelete( eventoId, { new: true })
+
+        res.json({
+            ok: true,
+            evento: eventoEliminado
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        })
+    }
 }
 
 module.exports = { //** Estos los encontramos el routes>events, los encontramos en las rutas del navegador, aqui definimos */
