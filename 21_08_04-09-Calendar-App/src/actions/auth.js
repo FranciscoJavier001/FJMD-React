c8ee22502cae87954430586b772694b3de234406
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2021-12-29 15:30:27 -0600

    Formulario

---
+++ 21_08_04-09-Calendar-App/src/actions/auth.js
@@ -0,0 +1,8 @@
+//** Funcion que comienza proceso de autentificacion */
+export const startLogin = ( email, password ) => { //** Exporto la funcion que va a ser asyncrona */
+    return async() => { //** Por el thunk se va a volver a disparar */
+        
+        // console.log(email, password); //** Para mostrarlo en consola pero desde aqui */
+
+    }
+}
--- 21_08_04-09-Calendar-App/src/components/auth/LoginScreen.js
+++ 21_08_04-09-Calendar-App/src/components/auth/LoginScreen.js
@@ -1,75 +1,104 @@
 import React from 'react';
+import { useDispatch } from 'react-redux'
+import { startLogin } from '../../actions/auth';
+import { useForm } from '../../hooks/useForm';
+
 import './login.css'; //** Esto es porque importamos info desde nuestro css */
 
-export const LoginScreen = () => { //** Esto es porque lo exportamos al archivo de rutas */
+export const LoginScreen = () => { //** Esto es porque lo exportamos al archivo de routes>AppRouter */
+
+    const dispatch = useDispatch() //** Hay que hacer el dispatch para la funcion startLogin de actions>auth */
+
+    const [ formLoginValues, handleLoginInputChange ] = useForm({ //** Es de hooks>useForm, cambiamos nombres, porque tendre 2 forms */
+        lEmail: 'francisco@mail.com', //** La "l", es porque viene del login */
+        lPassword: '123456' //** Es un usuario real de Postman */
+    });
+
+    const { lEmail, lPassword } = formLoginValues; //** Voy a extraer estas dos variables, vienen de formLoginValues */
+    const handleLogin = ( e ) => { //** Linea 32, funcion que se hace al hacer click */
+        e.preventDefault() //** Para que no recarge la pagina */
+
+        // console.log(formLoginValues); //** Para ver lo que trae el formulario al hacer click en login */
+        dispatch( startLogin( lEmail, lPassword )  ) //** Aqui voy a disparar startLogin, recibe email y password */
+    }
+
     return (
-        <div className="container login-container"> {/* El Container es el contenedor dentro de la pantalla, para que no abarque todo el espacio de la pantalla, y el login-container es la clase que establecimos en el CSS de el margen superior que tiene en la pantalla */}
-            <div className="row"> {/* Esto de Row quiere decir que solo va en una fila */}
-                <div className="col-md-6 login-form-1"> {/* El col-md-6 nos dice lo que va a medir el formulario de ingreso y lo demas es la clase que ya definimos */}
-                    <h3>Ingreso</h3> 
-                    <form>
-                        <div className="form-group"> {/* Es un formulario en grupo, osea aqui el correo, pero solo va un espacio */}
-                            <input //** Aqui va la info que va por el formulario y luego lo cerramos */
-                                type="text"
-                                className="form-control" //** Este es todo el espacio, es un formulario de control, donde va el texto */
-                                placeholder="Correo"
+        //** Container no abarca todo el espacio de la pantalla, login-container(clase) */
+        <div className="container login-container">
+            <div className="row"> {/* Ingreso y Registro misma fila */}
+                {/* El col-md-6 nos dice lo que va a medir el formulario de ingreso y lo demas es la clase que ya definimos */}
+                <div className="col-md-6 login-form-1">
+                    <h3>Ingreso</h3> {/* Titulo de arriba */}
+                    <form onSubmit={ handleLogin }> {/* Que va a ser un formulario, y el onSubmit va a tener un metodo */}
+                        <div className="form-group"> {/* Es un formulario en grupo, osea aqui el correo, pero solo va correo */}
+                            <input //** Aqui va la info que va por el formulario */
+                                type="text" //** Tipo de campo */
+                                className="form-control" //** Es el espacio, donde va el texto, (clase_Bootstrap) */
+                                placeholder="Correo" //** Lo que viene en gris dentro de la caja */
+                                name="lEmail" //** Necesito el email del campo que voy a recibir en el login, useForm */
+                                value={ lEmail } //** El valor va a ser el lEmail que recibi */
+                                onChange={ handleLoginInputChange } //** Esta es cuando se hacen los cambios */
                             />
                         </div>
-                        <div className="form-group"> {/* Creamos otro formulario pero con lo del password */}
+                        <div className="form-group"> {/* Creamos otro formulario pero solo con lo del password */}
                             <input
-                                type="password"
-                                className="form-control" //** Este es todo el espacio, es un formulario de control, donde va el texto */
+                                type="password" //** Para que se vean estrellitas */
+                                className="form-control"
                                 placeholder="Contraseña"
+                                name="lPassword" //** Aqui recibo el Password que recibe el useForm */
+                                value={ lPassword } //** Lo que viene en el lPassword */
+                                onChange={ handleLoginInputChange } //** Esta es cuando se hacen los cambios */
                             />
                         </div>
-                        <div className="form-group"> {/* Igual creamos otro formulario, osea hay que crear uno de cada uno independiente */}
-                            <input
-                                type="submit"
-                                className="btnSubmit" //**--  col-md-6 -- Lo primero es una clase de bootstrap, lo segundo es la distancia que manera en el form-roup */
-                                value="Login" 
+                        <div className="form-group">
+                            <input 
+                                type="submit" //** Esto significa que se puede hacer click */
+                                className="btnSubmit" /* btnSubmit(clase bootstrap) */
+                                value="Login" //** Lo que dice el Boton */
                             />
                         </div>
                     </form>
                 </div>
 
-                <div className="col-md-6 login-form-2"> {/* Ya entendi que el col-md-x es la distancia que va a tener de las columnas en bootstrap, luego lo otro es la clase que va a manejar */}
+                <div className="col-md-6 login-form-2"> {/* col-md-x= ancho container bootstrap (max_12)-login-form-2(clase_auth>login) */}
                     <h3>Registro</h3>
-                    <form>
-                        <div className="form-group"> {/* Entonces entendi que el form-group es donde va ordenado el formulario de control */}
+                    <form> {/* Es otro formulario */}
+                        <div className="form-group"> {/* Son las cajitas de texto */}
                             <input
                                 type="text"
-                                className="form-control" //** Este es todo el espacio, es un formulario de control, donde va el texto, con esto le damos estilos */
+                                className="form-control"
                                 placeholder="Nombre"
                             />
                         </div>
-                        <div className="form-group"> {/* Entonces entendi que el form-group es donde va ordenado el formulario de control */}
+                        <div className="form-group">
                             <input
                                 type="email"
-                                className="form-control" //** Este es todo el espacio, es un formulario de control, donde va el texto, con esto le damos estilos */
+                                className="form-control"
                                 placeholder="Correo"
                             />
                         </div>
-                        <div className="form-group"> {/* Entonces entendi que el form-group es donde va ordenado el formulario de control */}
+                        <div className="form-group">
                             <input
                                 type="password"
-                                className="form-control" //** Este es todo el espacio, es un formulario de control, donde va el texto, con esto le damos estilos */
-                                placeholder="Contraseña" 
+                                className="form-control"
+                                placeholder="Contraseña"
                             />
                         </div>
 
-                        <div className="form-group"> {/* Entonces entendi que el form-group es donde va ordenado el formulario de control */}
+                        <div className="form-group">
                             <input
                                 type="password"
-                                className="form-control" //** Este es todo el espacio, es un formulario de control, donde va el texto, con esto le damos estilos */
-                                placeholder="Repita la contraseña" 
+                                className="form-control"
+                                placeholder="Repita la contraseña"
                             />
                         </div>
 
-                        <div className="form-group"> {/* Entonces entendi que el form-group es donde va ordenado el formulario de control */}
-                            <input 
-                                type="submit" 
-                                className="btnSubmit" //** Asi le dimos estilos a los botones */
-                                value="Crear cuenta" />
+                        <div className="form-group">
+                            <input
+                                type="submit"
+                                className="btnSubmit"
+                                value="Crear cuenta"
+                            />
                         </div>
                     </form>
                 </div>
