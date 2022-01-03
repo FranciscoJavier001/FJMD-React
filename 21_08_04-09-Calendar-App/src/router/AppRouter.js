d9cec51cc8893ce90d52765376312e80dd167a1b
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-01-03 00:47:02 -0600

    startChecking

--- 21_08_04-09-Calendar-App/src/router/AppRouter.js
+++ 21_08_04-09-Calendar-App/src/router/AppRouter.js
@@ -15,19 +15,19 @@ import { startChecking } from '../actions/auth';
 
 export const AppRouter = () => {
 
-    const dispatch = useDispatch() //** Para lanzar el dispatch */
+    const dispatch = useDispatch() //** Para lanzar el dispatch, pero exactamente cuando entren a la app */
 
     useEffect(() => { //** Es para ponerme al tiro de los cambios, aqui lo pusimos porque era importante */
         
-        dispatch( startChecking() ) //** Esta pendiente del startChecking de types>typess */
+        dispatch( startChecking() ) //** Esta pendiente del startChecking de types>typess, cuendo entres te avisa token */
     }, [ dispatch ]) //** Lanzame el dispatch de la accion */
 
     return (
         <Router>
             <div>
                 <Switch>
-                    <Route exact path="/login" component={ LoginScreen } /> {/* Aqui voy a poner una ruta que va a ser exacta, con la direccion y el componente que vquiero mostrar */}
-                    <Route exact path="/" component={ CalendarScreen } />
+                    <Route exact path="/login" component={ LoginScreen } /> {/* Ruta exacta con el componente que quiero mostrar */}
+                    <Route exact path="/" component={ CalendarScreen } /> {/* Si no tiene nada mandalo al CalendarScreen */}
 
                     <Redirect to="/" />{/* Este lo uso para redireccionar al / */}
                 </Switch>
