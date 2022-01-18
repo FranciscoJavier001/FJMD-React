10a633808cd4b00779900bcea330edc6282830af
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-01-17 23:11:03 -0600

    test auth email incorrecto

--- 21_08_04-09-Calendar-App/src/test/actions/auth.test.js
+++ 21_08_04-09-Calendar-App/src/test/actions/auth.test.js
@@ -1,11 +1,16 @@
 //**_______________________________________________________________________________________________________________________________________________*/
 import configureStore from 'redux-mock-store'
 import thunk from 'redux-thunk' //** La de arriba ocupa esta porque las acciones retornan una funcion */
+import Swal from 'sweetalert2' //** La importo porque de aqui voy a sacar el error */
 
 import '@testing-library/jest-dom' //** Ayuda del tipado */
 
 import { startLogin } from '../../actions/auth'
 import { types } from '../../types/types'
+
+jest.mock( 'sweetalert2', () => ({ //** Mock de sa2, el mock es para decirle "cuando se llame", parentesis es para decirle que regresa un objeto */
+    fire: jest.fn() //** Que funcion estoy esperando que se llame en este caso el fire, que debe ser un jest.fn() */
+})) 
 
 const middlewares = [ thunk ] //** No recuerdo los middlewares */
 const mockStore = configureStore( middlewares )
@@ -44,5 +49,26 @@ describe('Pruebas en las acciones Auth', () => {
         // token = localStorage.setItem.mock.calls[0][1] //** Asi tengo el token */
         // console.log(localStorage.setItem.mock.calls[0][1]); //** Para saber que trae cuando es llamado que es token y t-i-d como arreglo */
     })
-    
+
+    test('startLogin incorrecto', async() => { //** Con un login incorrecto */
+        
+        await store.dispatch( startLogin('francisco@mail.com', '123456789') ) //** Lo que vamos a mandar en el startLogin */
+        let actions = store.getActions() //** Estas son un objeto vacio, porque no se disparo nada porque esta mal el password, let por reasignar */
+
+        // console.log(actions);
+
+        expect( actions ).toEqual([]) //** Voy a esperar recibir un objeto vacio, porque no pudo ingresar */
+
+        //** Voy a preguntar si el sa2 se ha llamado, el with es para mostrar en consola los errores con los que se ha llamado */
+        expect( Swal.fire ).toHaveBeenCalledWith( "Error", "Password incorrecto", "error" ) //** Asi se llamo y es con el password incorrecto */
+
+        //** Ahora toca con el email incorrecto */
+        await store.dispatch( startLogin('francisco1@mail.com', '123456') ) //** Lo que vamos a mandar en el startLogin */
+        actions = store.getActions() //** Estas son un objeto vacio, porque no se disparo nada porque esta mal el password */
+
+        // console.log(actions);
+
+        //** Se llamo y es con el email incorrecto */
+        expect( Swal.fire ).toHaveBeenCalledWith( "Error", "El usuario no existe con ese email", "error" ) 
+    })  
 })
