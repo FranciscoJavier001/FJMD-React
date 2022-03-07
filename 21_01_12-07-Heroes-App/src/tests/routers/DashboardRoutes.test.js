62e560dbb110a39a0cc5a172ef675d789cf78da4
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-03-06 19:22:39 -0600

    Pruebas en DashboardRoutes

--- 21_01_12-07-Heroes-App/src/tests/routers/DashboardRoutes.test.js
+++ 21_01_12-07-Heroes-App/src/tests/routers/DashboardRoutes.test.js
@@ -1,3 +1,4 @@
+//**_______________________________________________________________________________________________________________________________________________*/
 import React from 'react'
 import { mount } from 'enzyme'
 import { AuthContext } from '../../auth/AuthContext'
@@ -6,8 +7,6 @@ import { MemoryRouter } from 'react-router-dom'
 
 describe('Pruebas en <DashboardRoutes />', () => {
     
-    // Si creemos que esto se va ausar mucho en las pruebas hay que dejarlo afuera
-    //** Esto es lo que tenia el context, es una simulacion del objeto del provider, el dispatch es una funcion que la vamos a provar con el jest y el estado inicial de la aplicacion del logged es false */
     const contextValue = {
         dispatch: jest.fn(),
         user: {
@@ -18,11 +17,8 @@ describe('Pruebas en <DashboardRoutes />', () => {
 
     test('Debe mostrarse correctamente', () => {
         
-        // Que haga match con el snapshot
         const wrapper = mount(
-            //** Esto es el conuwtextValue, que se le va a proveer al AuthContext, de igual manera con el mount voy a renderizar el AppRouter para las pruebas */
             <AuthContext.Provider value={ contextValue }>
-                {/* Recuerda, para que no nos salga el error fatal, hay que envolver esto en el MemoryRouter */}
                 <MemoryRouter>
                     <DashboardRoutes />
                 </MemoryRouter>
@@ -30,6 +26,6 @@ describe('Pruebas en <DashboardRoutes />', () => {
         )
 
         expect( wrapper ).toMatchSnapshot()
-        expect( wrapper.find('.text-info').text().trim() ).toBe('Pedro') //** Asi confirmamos que el usuario autentificado sea pedro, y el trim es para borrar espacios */
+        expect( wrapper.find('.text-info').text().trim() ).toBe('Pedro') 
     })  
 })
