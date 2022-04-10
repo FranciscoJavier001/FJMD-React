251af320a9f714045ffd5f29b7e27e6b33e82d0e
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-04-10 01:24:54 -0500

    Creacion de Carpetas

---
+++ my-app/src/index.js
@@ -0,0 +1,11 @@
+//**_______________________________________________________________________________________________________________________________________________*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Pokemon } from './Pokemon';
+
+import './index.css';
+
+ReactDOM.render(
+    <Pokemon />,
+  document.getElementById('root')
+);
