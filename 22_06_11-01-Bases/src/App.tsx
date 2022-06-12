8652e3af9bd7ca919dface2ca284a359c7c4c0ff
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-06-12 18:05:08 -0500

    Antes de la Tarea

--- 22_06_11-01-Bases/src/App.tsx
+++ 22_06_11-01-Bases/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Counter } from './bases/Counter';
+import { CounterBy } from './bases/CounterBy';
 
 function App() {
   return (
     <>
       {/* <Counter initialValue={15} /> {/* Pero aqui puedo definirle un valor en duro, sino lo pongo lo manda por default el 0 del Counter */}
-      <Counter /> {/* Sino pao el valor se queda en 0 */}
+      {/* <Counter /> {/* Sino pao el valor se queda en 0 */}
+      <CounterBy />
     </>
   );
 }
---
+++ 22_06_11-01-Bases/src/bases/CounterBy.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useState } from 'react';
+
+interface CounterProps { //** Propiedades que le vamos a pasar al Counter */
+    initialValue?: number; //** Simpemente le asignamos el tipo number y por default es 0, con el ? le decimos que es opcional */
+}
+
+//** Lo hago con rafc -- Para pasarle un valor aqui voy a desestructurar esa variable y asigno valor numero */
+export const CounterBy = ( {initialValue = 0 }: CounterProps ) => { 
+
+  //** Primero el estado actual, segundo el que modifica el estado, tercero valor inicial */
+  const [counter, setCounter] = useState({ //** Lo ultimo va a tener el valor del initialValue */
+    counter: initialValue, //** Asi inicializamos el valor del contador, con el valor inicial del initialValue */
+    clicks: 0, //** Asi voy a saber cuantas veces he aumentado ese contador */
+  })
+
+  const handleClick = () => { //** Funcion que se hace al apretar un boton */
+      setCounter( valIni => valIni + ) //** La accion que se hace al hacer click */
+      // console.log('click'); //** Muestro en Consola cuando haga click */
+  }
+
+  return ( //** Muestro en el DOM */
+    <> {/* Es un div que esta vacio */}
+    <h1>Counter: { counter }</h1> {/* Muestro en el DOM el contador */}
+    <h1>Clicks: { clicks }</h1> {/* Tambien quiero mostrar los clicks */}
+
+    <button //** Boton */
+    onClick={ () => handleClick(1) }> {/* Cuando haga click */}
+        +1
+    </button>
+
+    <button //** Boton */
+    onClick={ () => handleClick(5) }> {/* Cuando haga click */}
+        +5
+    </button>
+    </>
+  )
+}
