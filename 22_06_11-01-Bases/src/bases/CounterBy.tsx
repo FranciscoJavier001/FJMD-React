8722a9f326c82cd9304bdf65feca860cc064556c
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-06-12 18:19:16 -0500

    Codigo Funcionando... falta optimizacion

--- 22_06_11-01-Bases/src/bases/CounterBy.tsx
+++ 22_06_11-01-Bases/src/bases/CounterBy.tsx
@@ -9,13 +9,20 @@ interface CounterProps { //** Propiedades que le vamos a pasar al Counter */
 export const CounterBy = ( {initialValue = 0 }: CounterProps ) => { 
 
   //** Primero el estado actual, segundo el que modifica el estado, tercero valor inicial */
-  const [counter, setCounter] = useState({ //** Lo ultimo va a tener el valor del initialValue */
+  const [counterState, setCounterState] = useState({ //** Lo ultimo va a tener el valor del initialValue */
     counter: initialValue, //** Asi inicializamos el valor del contador, con el valor inicial del initialValue */
     clicks: 0, //** Asi voy a saber cuantas veces he aumentado ese contador */
   })
 
-  const handleClick = () => { //** Funcion que se hace al apretar un boton */
-      setCounter( valIni => valIni + ) //** La accion que se hace al hacer click */
+  //** Voy a desestructurar un objeto, para no tener error en el counter ni clicks */
+  const { counter, clicks } = counterState;
+
+  const handleClick = ( value: number) => { //** Funcion que se hace al apretar un boton, recibo un valor del tipo numero */
+    setCounterState( valIni => ({ //** La accion que se hace al hacer click, voy a retornar directamete un objeto */
+        //** Dentro del objeto va a venir un counter en el valor anterior valIni.counter y le sumo el value que recibo arriba */
+        counter: valIni.counter + value,
+        clicks: valIni.clicks + 1, //** Como es typeScript debo definir todas las variables y lo que hacens */
+      })) 
       // console.log('click'); //** Muestro en Consola cuando haga click */
   }
 
