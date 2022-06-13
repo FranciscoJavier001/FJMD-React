70d8de5c3a7924b07cb8be1dac2a17b9e6952520
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-06-12 21:46:12 -0500

    Informacin del useEffect

--- 22_06_11-01-Bases/src/App.tsx
+++ 22_06_11-01-Bases/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 // import { Counter } from './bases/Counter';
-import { CounterBy } from './bases/CounterBy';
+// import { CounterBy } from './bases/CounterBy';
+import { CounterEffect } from './bases/CounterEffect';
 
 function App() {
   return (
     <>
       {/* <Counter initialValue={15} /> {/* Aqui defino un valor en duro, sino lo pongo lo manda por default el 0 del Counter */}
       {/* <Counter /> {/* Sino pongo el valor se queda en 0 */}
-      <CounterBy /> {/* Lo que renderizo, ya viene los valores desde el Componente */}
+      {/* <CounterBy /> {/* Lo que renderizo, ya viene los valores desde el Componente */}
+      < CounterEffect />
     </>
   );
 }
--- 22_06_11-01-Bases/src/bases/CounterEffect.tsx
+++ 22_06_11-01-Bases/src/bases/CounterEffect.tsx
@@ -1,23 +1,25 @@
 import React from 'react'
-import { useState } from 'react'; //** Hook del uS */
+import { useState, useEffect } from 'react'; //** Hooks */
 
-interface CounterProps { //** Definicion y tipado de las variables del CP */
-  initialValue ? : number; //** iV es opcional que tenga un valor numerico */
-}
+//** rafc */
+export const CounterEffect = () => { 
 
-//** rafc, Desestructuramos el iV y le asignamos un valor numerico, definido en las CP */
-export const Counter = ( {initialValue = 0 }: CounterProps ) => { 
+  const [counter, setCounter] = useState(5) //** 1=Estado Actual, 2=Modificacion estado, 3=Valor inicial del Hook */
 
-const [counter, setCounter] = useState(initialValue) //** 1=Estado Actual, 2=Modificacion estado, 3=Valor inicial del Hook */
-
-const handleClick = () => { //** Funciones del Boton */
+  const handleClick = () => { //** Funciones del Boton */
     setCounter( valIni => valIni + 1) //** Valores que recibe el sC, dentro instrucciones de la funcion */
-    // console.log('click'); //** Mostrar en consola al hacer click */
-}
+  }
+
+  useEffect(() => { //** uE dispara una funcion, siempre minimo 1 vez se dispara */
+    first
+    return () => { //** Funcion que se ejecuta cuando el componente va a ser destruido, conocido como funcion de limpieza */
+      second
+    }
+  }, [third]) //** Ultimo=Arreglo de dependencias, se ejecuta cuando cambie */
 
   return ( //** Renderizamos en el DOM */
     <> {/* Fragment=No contiene nada dentro */}
-    <h1>Counter: { counter }</h1> {/* Muestro el estado del Counter */}
+    <h1>CounterEffect: { counter }</h1> {/* Muestro el estado del Counter */}
 
     <button //** Boton */
     onClick={ () => handleClick() }> {/* Al hacer click mando llamar la funcion */}
