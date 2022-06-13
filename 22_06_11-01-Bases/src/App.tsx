9d1bc06198bb2e57b20193f9bbdd41454fc29481
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-06-12 20:53:56 -0500

    Counter - Funcionando y Limpio

--- 22_06_11-01-Bases/src/App.tsx
+++ 22_06_11-01-Bases/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <>
       {/* <Counter initialValue={15} /> {/* Aqui defino un valor en duro, sino lo pongo lo manda por default el 0 del Counter */}
       {/* <Counter /> {/* Sino pongo el valor se queda en 0 */}
-      <CounterBy /> {/* Lo que renderizo, ya viene los valores definidos */}
+      <CounterBy /> {/* Lo que renderizo, ya viene los valores desde el Componente */}
     </>
   );
 }
