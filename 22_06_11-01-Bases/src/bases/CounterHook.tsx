d06fbebe9b6333b34ab3ac73ea8a3f8ac11fab27
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-06-20 13:12:53 -0500

    Documentacion

--- 22_06_11-01-Bases/src/bases/CounterHook.tsx
+++ 22_06_11-01-Bases/src/bases/CounterHook.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { useCounter } from '../hooks/useCounter'
+import { useCounter } from '../hooks/useCounter' //** Importamos este CH que creamos, para poder usar sus variables */
 
 //** rafc */
-export const CounterHook = () => { //** Esto lo usamos para poder importarlo al app */
+export const CounterHook = () => { //** Esto lo usamos para poder exportarlo al app */
 
   //** Asi voy a usar el customHook y asi ya tengo acceso a los elementos que necesitaba, al igual que el useCounter */
-  const { counter, counterElement, handleClick } = useCounter() 
+  const { counter, elementToAnimate, handleClick } = useCounter({ maxCount: 15}) //** Defino valor maximo definitivo */
+
   return ( //** Renderizamos en el DOM */
     <> {/* Fragment=No contiene nada dentro */}
-      <h1>Counter Hook:</h1>
-      <h2 ref={ counterElement }>{ counter }</h2> {/* Le voy a asignar una referencia que defini arriba */}
+      <h1>Counter Hook:</h1> {/* Titulo */}
+      <h2 ref={ elementToAnimate }>{ counter }</h2> {/* Le asigno una referencia, al elemento animado y muestro el contador */}
 
       <button //** Boton */
-      onClick={ () => handleClick() }> {/* Al hacer click mando llamar la funcion */}
+      onClick={ handleClick }> {/* Al hacer click mando llamar la funcion */}
           +1
       </button>
     </>
--- 22_06_11-01-Bases/src/hooks/useCounter.ts
+++ 22_06_11-01-Bases/src/hooks/useCounter.ts
@@ -1,37 +1,41 @@
 //** Lo inicializamos con rafc */
-import React from 'react'
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect, useLayoutEffect } from 'react'; //** Hooks que voy a utilizar */
 import { gsap } from 'gsap'; //** Importamos este paquete para animaciones */
 
-const MAXIMIN_COUNT = 10 //** Lo voy a recibir como argumento despues */
+//** Me creo una interface para los hooks */
 
-export const useCounter = () => {
+//** Voy a desestructurar un objeto que voy a mandar, aqui le asigno un valor, pero va a respetar el del cH */
+export const useCounter = ({ maxCount = 0 }) => { //** Para poder utilizar esta dependencia necesito un numero */
 
     const [counter, setCounter] = useState(5) //** 1=Estado Actual, 2=Modificacion estado, 3=Valor inicial del Hook */
-    //** Este va a ser el objeto de referenca, con HHE seleccion elemento del DOM e inicializo null */
-    const counterElement = useRef<HTMLHeadingElement>(null)
+    //** eTA va a tener una referencia any(cualquier elemento de HTML) y recibe un valor, pero lo puedo dejar vacio o en null */
+    const elementToAnimate = useRef<any>() 
+
+    const tl = useRef( gsap.timeline() ) //** Voy a crearme otro uR con la referencia al gsap con un linea de tiempo */
     
     const handleClick = () => { //** Funciones del Boton */
-      //** Valores que recibe el sC, dentro definimos la funcion del valor inicial aumente en 1 y que no pase del M_C */
-      setCounter( valIni => Math.min( valIni + 1, MAXIMIN_COUNT ) ) //** Asi ya no puede subir mas */
+      //** sC tiene n valor inicial, que es el minimo y al precionar el boton aumenta en 1 y el valor maximo es el mC, si pasa bloqueo */
+      setCounter( prev => Math.min( prev + 1, maxCount ) ) //** Que no pueda subir mas del mC */
     }
+
+    //** Se asegura que ya tengamos contruidos nuestros elementos html y las dimenciones establecidas */
+    useLayoutEffect(() => {
+
+        if( !elementToAnimate.current ) return; //** Hago una evaluacion en caso que no tenga un valor y lo que retorna */
+        
+        //** Recuerda.. tenemos el to y el from, pause para que no se inicie */
+        tl.current.to( elementToAnimate.current, { y: -10, duration: 0.2, ease: 'ease.out' } ) //** Efecto de entrada current estado actual */
+                  .to( elementToAnimate.current, { y: 0, duration: 1, ease: 'bounce.out' } ) //** Efecto de salida */
+                  .pause() //** No quiero que se empiece a hacer la animacion */
+    }, []) //** Dejo dependencia vacia para que solo se construya 1 vez */
   
-    useEffect(() => { //** Que se haga este efecto cuando llegue a 10 */
-      if ( counter < 10 ) return //** Cuando el contador llegue a 10 se manda el mensaje en consola */
-      console.log('%cEl contador es mayor de 10', 'color: red; background-color: black'); //** Estilos para consola */
-  
-      const tl = gsap.timeline() //** Voy a crear un timelime */
-      
-      //** Recuerda.. tenemos el to y el from */
-      tl.to( counterElement.current, { y: -10, duration: 0.2, ease: 'ease.out' } ) //** Efecto de entrada */
-        .to( counterElement.current, { y: 0, duration: 1, ease: 'bounce.out' } ) //** Efecto de salida */
-  
-  }, [counter]) //** Cada vez que el counter cambia que se dispare */
+    useEffect(() => { //** Accion que se va a disparar */
+        tl.current.play(1.5) //** Cada vez que se dispare, quiero que inicie la animacion en la posicion inicial */
+    }, [counter]) //** Cada vez que el counter cambia que se dispare este evento de animacion */
   
     return { //** Lo pongo asi porque quiero retornar un objeto */
-        counter, //** Tenia errores en el  */
-        counterElement, 
-        // elementToAnimate: counterElement, //** Le cambio el nombre, pero con los : es donde va a apuntar */
+        counter, //** Son elementos que se van a utilizar y necesito exportatrlos CH */
+        elementToAnimate,
         handleClick
   }
 }
