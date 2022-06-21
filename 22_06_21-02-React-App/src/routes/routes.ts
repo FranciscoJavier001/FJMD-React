364b4f0c04dc4174ec128cf4578c7255ddb0d954
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-06-21 15:30:45 -0500

    Antes de Rutas Estaticas

---
+++ 22_06_21-02-React-App/src/routes/routes.ts
@@ -0,0 +1,31 @@
+import { Component } from "react";
+
+import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
+
+interface Route {
+    to: string,
+    path: string,
+    Component: () => JSX.Element, //** Para saber de que tipo es dejo cursos y lo copio */
+    name: string
+}
+
+export const routes: Route[] = [ //** Esta ruta es un arreglo de Route */
+    {
+        to: '/lazy1',
+        path: '/lazy1',
+        Component: LazyPage1,
+        name: 'Lazy-1'
+    },
+    {
+        to: '/lazy2',
+        path: '/lazy2',
+        Component: LazyPage2,
+        name: 'Lazy-2'
+    },
+    {
+        to: '/lazy3',
+        path: '/lazy3',
+        Component: LazyPage3,
+        name: 'Lazy-3'
+    }
+]
