8bcd55f492f9e7123304c497cdedc2cafcfae35c
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-06-24 11:36:02 -0500

    Muestro la imagen y sus propiedades

--- 22_06_21-02-React-App/src/02-component-patterns/components/ProductCard.tsx
+++ 22_06_21-02-React-App/src/02-component-patterns/components/ProductCard.tsx
@@ -3,17 +3,29 @@ import { useProduct } from '../hooks/useProduct';
 import styles from '../styles/styles.module.css' //** Importamos los estilos y defino ese nombre */
 import noImage from '../assets/no-image.jpg' //** Importo la imagen */
 
-export const ProductCard = () => { //** rafc */
+interface Props { //** Aqui voy a definir las properties */
+  product: Product //** Voy a recibir una property que va a ser product de tipo Product(Definido abajo) */
+}
+
+interface Product { //** Voy a definir las caracteristicas del Objeto */
+  id: string;
+  title: string;
+  img?: string //** ?-Para decirle que es opcional */
+}
+
+//** Dentro de los ({}) voy a desestructirar el product para obligar a recibirlo -Tengo las properties con : puedo modificar las props */
+export const ProductCard = ({ product }: Props) => {
 
     const { counter, increaseBy } = useProduct() //** Solo mando llamar el Hook - Desestructuro lo que retorna entre llaves */
 
 // console.log(styles); //** Para confirmar que importe los estilos */
   return ( //** Renderizar */
     <div className={ styles.productCard }> {/* Definir la clase de la tarjeta */}
-        <img className={ styles.productImg } src="./coffee-mug.png" alt="Coffe Mug" />
-        {/* <img className={ styles.productImg } src={ noImage } alt="No Found" /> */}
 
-        <span className={ styles.productDescription }>Coffee Mug</span>
+        {/* Si en product.img viene product.img usa esa, si no viene nada entonces usa la imagen vacia */}
+        <img className={ styles.productImg } src={ product.img ? product.img : noImage } alt="Coffee Mug" />
+
+        <span className={ styles.productDescription }>{ product.title }</span> {/* Desestructuro el nombre del titulo del producto */}
 
         <div className={ styles.buttonsContainer }> {/* Asi pongo los estilos */}
             <button className={ styles.buttonMinus }
--- 22_06_21-02-React-App/src/02-component-patterns/pages/ShoppingPage.tsx
+++ 22_06_21-02-React-App/src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,11 +1,25 @@
 import { ProductCard } from "../components/ProductCard"
 
+const product = { //** Esto va a ser un objeto, defino la interface en PC, va a tener variables necesarias-En Duro */
+  id: "1",
+  title: "Coffee Mug - Card",
+  img: "./coffee-mug.png"
+}
+
 export const ShoppingPage = () => { //** rafc */
-  return (
+  return ( 
     <div>
         <h1>Shopping Store</h1>
-        <hr />
-        <ProductCard />
+        <hr /> {/* Linea */}
+
+        <div style={{ //** Le voy a dar estilos a este div */
+          display: "flex", //** Se queden las tarjetas en fila */
+          flexDirection: "row", //** Que la direccion este en fila */
+          flexWrap: "wrap", //** Para que todos sigan la linea y si no caben, que vallan para abajo */
+        }}>
+
+          <ProductCard product={ product } /> {/* Le mando el producto que tengo aqui y le mando el mismo */}
+        </div>
     </div>
   )
 }
