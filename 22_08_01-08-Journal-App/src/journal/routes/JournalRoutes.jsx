01d80754fa9c984195101b2e51bcd1489afcbb5e
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-08-02 13:47:40 -0500

    Import Packages from React-Router-Dom-6

--- 22_08_01-08-Journal-App/src/journal/routes/JournalRoutes.jsx
+++ 22_08_01-08-Journal-App/src/journal/routes/JournalRoutes.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+import { JournalPage } from '../pages/JournalPage'
 
 export const JournalRoutes = () => {
   return (
