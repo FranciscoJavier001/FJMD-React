99c3f0b23c94a6a64dd37ddf276adab8b6c15c51
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-08-02 13:26:05 -0500

    BrowserRouter Main App

--- 22_08_01-08-Journal-App/src/main.jsx
+++ 22_08_01-08-Journal-App/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+
 import { JournalApp } from './JournalApp'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <JournalApp />
+    <BrowserRouter>
+      <JournalApp />
+    </BrowserRouter>
   </React.StrictMode>
 )
