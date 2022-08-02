854057c980bbfe82529035b2978e177de49d08b4
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-08-02 13:21:12 -0500

    Initialization Main Routes

--- 22_08_01-08-Journal-App/src/JournalApp.jsx
+++ 22_08_01-08-Journal-App/src/JournalApp.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { AppRouter } from './router/AppRouter'
 
 export const JournalApp = () => {
   return (
     <>
-        <h1>Journal App</h1>
+        <AppRouter />
     </>
   )
 }
