b0bafacaab79152e6e627a21269d7fdb79388608
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-08-04 16:57:05 -0500

    Implementation MaterialUI

--- 22_08_01-08-Journal-App/src/journal/pages/JournalPage.jsx
+++ 22_08_01-08-Journal-App/src/journal/pages/JournalPage.jsx
@@ -1,7 +1,9 @@
 import React from 'react' //** FC */
+import { Typography } from "@mui/material"
 
 export const JournalPage = () => {
   return (
-    <div>JournalPage</div>
+    <Typography variant="h1">JournalPage</Typography> //** Debemos de usar el Typography ejemplo tipo h1 */
+    
   )
 }
