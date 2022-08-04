0f7f458a1f244fda3b3e4ad2d2dc63682d303955
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-08-04 16:51:35 -0500

    AppTheme Renderization

--- 22_08_01-08-Journal-App/src/JournalApp.jsx
+++ 22_08_01-08-Journal-App/src/JournalApp.jsx
@@ -1,11 +1,12 @@
 //** FC */
 import React from 'react'
 import { AppRouter } from './router/AppRouter'
+import { AppTheme } from './theme'
 
 export const JournalApp = () => {
   return (
-    <>
+    <AppTheme> {/* Envuelvo la aplicacion dentro de este tema */}
         <AppRouter /> {/* La que vamos a renderizar, esta solo manda login o register */}
-    </>
+    </AppTheme>
   )
 }
