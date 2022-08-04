c70270faa2ea607d356bc6e4b4b4c2c088ea83bd
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-08-04 16:00:20 -0500

    AppTheme HOC

--- 22_08_01-08-Journal-App/src/theme/AppTheme.jsx
+++ 22_08_01-08-Journal-App/src/theme/AppTheme.jsx
@@ -1,9 +1,11 @@
-//** FC */
+//** HOC, componente que recibe otros componentes hijos */
 import React from 'react'
 
-export const AppTheme = () => {
+export const AppTheme = ({ children }) => { //** Es HOC porque recibe el children */
   return (
-    <>
-    </>
+    <ThemeProvider theme={ theme }>
+        <CssBaseline />
+        { children } {/* Este va a ser el App */}
+    </ThemeProvider>
   )
 }
