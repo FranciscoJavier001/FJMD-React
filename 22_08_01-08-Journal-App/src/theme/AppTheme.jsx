524ab1a2f2d211a2856b5ad8852863a0145edf54
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-08-04 16:40:16 -0500

    Importation purpleTheme

--- 22_08_01-08-Journal-App/src/theme/AppTheme.jsx
+++ 22_08_01-08-Journal-App/src/theme/AppTheme.jsx
@@ -1,9 +1,13 @@
 //** HOC, componente que recibe otros componentes hijos */
 import React from 'react'
+import { ThemeProvider } from '@emotion/react'
+import { CssBaseline } from '@mui/material'
+
+import { purpleTheme } from './purpleTheme'
 
 export const AppTheme = ({ children }) => { //** Es HOC porque recibe el children */
   return (
-    <ThemeProvider theme={ theme }>
+    <ThemeProvider theme={ purpleTheme }> {/* Le asigno cual va a ser el tema y lo importo */}
         <CssBaseline />
         { children } {/* Este va a ser el App */}
     </ThemeProvider>
