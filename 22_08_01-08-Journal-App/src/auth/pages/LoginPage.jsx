d9902b3ba16cd428078b2bbeb1781d0d02f9f74e
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-08-05 10:25:48 -0500

    All screen purple(primary color)

--- 22_08_01-08-Journal-App/src/auth/pages/LoginPage.jsx
+++ 22_08_01-08-Journal-App/src/auth/pages/LoginPage.jsx
@@ -1,7 +1,17 @@
 import React from 'react' //** Solamente es un FC */
+import { Grid } from '@mui/material'
 
 export const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <Grid
+      container
+      spacing={ 0 }
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
+      >
+      
+    </Grid>
   )
 }
