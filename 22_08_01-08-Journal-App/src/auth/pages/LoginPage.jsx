25a4ea1c9eeb42601381a61dae7b8fd19becaf3a
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-08-06 20:16:18 -0500

    Layout

--- 22_08_01-08-Journal-App/src/auth/pages/LoginPage.jsx
+++ 22_08_01-08-Journal-App/src/auth/pages/LoginPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react' //** Solamente es un FC */
-import { Grid } from '@mui/material'
+import { Grid, TextField, Typography } from '@mui/material'
 
 export const LoginPage = () => {
   return (
@@ -9,9 +9,41 @@ export const LoginPage = () => {
       direction="column" //** Direccio es columnas, osea hacia abajo */
       alignItems="center" //** Centrado de Items en medio */
       justifyContent="center" //** Contenido centrado a la mitad */
-      sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }} //** sx=Tengo acceso al theme.provider, src/theme/purpleTheme */
+      sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }} //** stylextra=Tengo acceso al theme.provider, src/theme/purpleTheme */
       >
         
+        <Grid item /* Caja de en medio */
+          className='box-shadw' //** Defino clase en styes.css, es una sobra */
+          xs={ 3 } //** Tamaño pantalla pequeña *
+          sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2 }}> {/* Para poder renderizar */}
+            <Typography variant="h5" sx={{ mb:1 }}>Login</Typography> {/* Variables asignables, margin, bajo, asigno un mensaje */}
+
+            <form> {/* Formulario */}
+              <Grid container> {/* Contenedor */}
+                <Grid item xs={ 12 } sx={{ mt: 2 }}> {/* Item, tamaño en pantalla chica y estilos extras */}
+                  <TextField /* Campo de texto */
+                    label="Email" //** Mensaje que se muestra en el campo */
+                    type="email" //** Tipo que es */
+                    placeholder='correo@email.com' //** Mensaje que sale al darle click */
+                    fullWidth //** Ancho completo */
+                    />
+                </Grid>
+
+                <Grid item xs={ 12 } sx={{ mt: 2 }}> {/* Item, tamaño en pantalla chica y estilos extras */}
+                  <TextField /* Campo de texto */
+                    label="Contraseña" //** Mensaje que se muestra en el campo */
+                    type="password" //** Tipo que es */
+                    placeholder='Contraseña' //** Mensaje que sale al darle click */
+                    fullWidth //** Ancho completo */
+                    />
+                </Grid>
+
+                
+              </Grid>
+            </form>
+
+
+        </Grid>
     </Grid>
   )
 }
