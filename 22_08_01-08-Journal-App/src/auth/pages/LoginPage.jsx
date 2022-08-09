e29324d992f0cee31ad955fd0925b93f79989a29
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-08-08 23:54:18 -0500

    Layout LoginPage

--- 22_08_01-08-Journal-App/src/auth/pages/LoginPage.jsx
+++ 22_08_01-08-Journal-App/src/auth/pages/LoginPage.jsx
@@ -1,5 +1,7 @@
 import React from 'react' //** Solamente es un FC */
-import { Grid, TextField, Typography } from '@mui/material'
+import { Link as RouterLink } from 'react-router-dom'
+import { Button, Grid, Link, TextField, Typography } from '@mui/material'
+import { Google } from '@mui/icons-material'
 
 export const LoginPage = () => {
   return (
@@ -37,7 +39,26 @@ export const LoginPage = () => {
                     fullWidth //** Ancho completo */
                     />
                 </Grid>
+
+                <Grid container spacing={ 2 } sx={{ mb:2, mt:1 }}>
+                  <Grid item xs={ 12 } sm={ 6 }>
+                    <Button variant="contained" fullWidth>
+                      Login
+                    </Button>
+                  </Grid>
+                  <Grid item xs={ 12 } sm={ 6 }>
+                    <Button variant="contained" fullWidth>
+                      <Google />
+                        <Typography sx={{ ml: 1}}>Google</Typography>
+                    </Button>
+                  </Grid>
+                </Grid>
                 
+                <Grid container direction="row" justifyContent="end">
+                  <Link component={ RouterLink } color="inherit" to="/auth/register">
+                    Create Accound
+                  </Link>
+                </Grid>
               </Grid>
             </form>
         </Grid>
