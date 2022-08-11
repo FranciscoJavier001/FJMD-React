8c31521a5260fb1b443790864fa91786b6b0e1b4
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-08-10 21:52:52 -0500

    New Variable and Annotations

--- 22_08_01-08-Journal-App/src/journal/layout/JournalLayout.jsx
+++ 22_08_01-08-Journal-App/src/journal/layout/JournalLayout.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
-import Box from '@mui/system/';
+import { Box } from '@mui/system/';
+
+const drawerWidth = 240;
 
 export const JournalLayout = ({ children }) => { //** Recibo el children */
   return (
     <Box sx={{ display: 'flex' }}> {/* Box=Caja, display:flex=Los elementos van a ir en fila */}
 
-      {/* Navbar */}
+      {/* Navbar drawerWidth */}
 
-      {/* Sidebar */}
+      {/* Sidebar drawerWidth */}
 
       <Box
         component='main'
-        sx={{ flexGrow: 1, p: 3 }} //** flexGrow=Todos tienen la misma anchura */
+        sx={{ flexGrow: 1, p: 3 }} //** flexGrow=Todos tienen la misma anchura, p=padding anchura de 3 hacia afuera */
         >
           {/* Toolbar */}
 
