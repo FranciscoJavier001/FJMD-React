f35276991aa606dfc2532844f685c6f1f2968d65
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-08-11 11:33:47 -0500

    Custom Design Navbar

--- 22_08_01-08-Journal-App/src/journal/components/Navbar.jsx
+++ 22_08_01-08-Journal-App/src/journal/components/Navbar.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { AppBar, IconButton, Toolbar } from '@mui/material'
+import { MenuOutlined } from '@mui/icons-material';
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <AppBar //** Es como un navbar */
+        position='fixed' //** Posicion Fija */
+        sx={{ }}
+    >
+        <Toolbar> {/* Similar a la tabla de Excell */}
+            <IconButton> {/* Botones con figuritas */}
+                <MenuOutlined /> {/* Menu fuera de la linea */}
+            </IconButton>
+        </Toolbar>
+    </AppBar> 
   )
 }
