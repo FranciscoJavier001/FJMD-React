65c21a97bed653d8fa6ada1ea492689d0688d5c4
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-08-11 18:19:16 -0500

    Navbar Customized and Annotations

--- 22_08_01-08-Journal-App/src/journal/layout/JournalLayout.jsx
+++ 22_08_01-08-Journal-App/src/journal/layout/JournalLayout.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import { Box } from '@mui/system/';
+import { Navbar } from '../components';
 
-const drawerWidth = 240;
+const drawerWidth = 240; //** Variable especial de asignacion espacio inicial en blanco (Solo funciona en este componente) */
 
-export const JournalLayout = ({ children }) => { //** Recibo el children */
+export const JournalLayout = ({ children }) => { //** Componente que recibe el children, renderizado en JournalPage */
   return (
-    <Box sx={{ display: 'flex' }}> {/* Box=Caja, display:flex=Los elementos van a ir en fila */}
+    <Box sx={{ display: 'flex' }}> {/* Box=Caja(Abarca todo el ancho), display:flex=(Elementos en la misma fila ) */}
 
-      {/* Navbar drawerWidth */}
+      <Navbar drawerWidth={ drawerWidth }/> {/* Importo Navbar, le asigno un tamaño del espacio inicial en blanco L5 */}
 
       {/* Sidebar drawerWidth */}
 
-      <Box
-        component='main'
-        sx={{ flexGrow: 1, p: 3 }} //** flexGrow=Todos tienen la misma anchura, p=padding anchura de 3 hacia afuera */
+      <Box 
+        component='main' //** Nombre del componente principal, como si fuera un div */
+        sx={{ flexGrow: 1, p: 3 }} //** flexGrow=Elemento que domina en anchura, p=padding 3, anchura del texto hacia afuera */
         >
           {/* Toolbar */}
 
-          {children} {/* Renderizo el children */}
+          {children} {/* Renderizo el children de JournalPage, que retorna JournalLayout con sus componentes dentro */}
 
       </Box>
 
