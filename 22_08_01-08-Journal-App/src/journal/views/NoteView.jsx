214855542dbb68a467e686d01ac524bb4eee0ca4
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-09-02 05:23:20 -0500

    Implemented New Save Button

--- 22_08_01-08-Journal-App/src/journal/views/NoteView.jsx
+++ 22_08_01-08-Journal-App/src/journal/views/NoteView.jsx
@@ -12,9 +12,16 @@ const noteDate = moment( date ) //** Esto es para mostrar la fecha exacta, lo us
     //** Grid me permite jugar con los elementos dentro de los elementos, box como div */
     /* Linea de Fecha y Boton Guardar, direccion como fila, justificacion esquinas, items en medio, margen hacia abajo */
     <Grid container direction="row" justifyContent='space-between' alignItems='center' sx={{ mb: 1}}> 
+        
         <Grid item> {/* Contenedor en linea, donde primero va la fecha */}
             {/* Va a ser la fecha, con el tamaño grande, el tipo de letra delgada y con esto muestro mes/dia/año */}
             <Typography fontSize={ 39 } fontWeight='light'>{ noteDate.format('LL') }</Typography> 
+        </Grid>
+        <Grid item> {/* Este item es del boton */}
+            <Button color="primary" sx={{ padding: 2 }}> {/* Color del boton y el pading del boton, de adentro hacia afuera */}
+                <SaveOutlined sx={{ fontSize: 30, mr: 1 }}/> {/* Icono del boon, tamaño del icono y margen hacia la derecha hacia letras */}
+                Save {/* Lo que dice */}
+            </Button>
         </Grid>
     </Grid>
   )
