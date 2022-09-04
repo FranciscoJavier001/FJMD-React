3f8f5a145cf5094101eeacd15b10d6a426c20b99
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-09-04 15:32:13 -0500

    Code Update

--- 22_09_04-09-Toolkit-Redux/src/App.jsx
+++ 22_09_04-09-Toolkit-Redux/src/App.jsx
@@ -8,9 +8,6 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
@@ -20,13 +17,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
