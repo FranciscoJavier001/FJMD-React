ba02fd0421e94621a2a2587be4ca1377ac3889ce
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-09-28 21:25:46 -0500

    Deleted React Logo

--- 22_09_04-09-Toolkit-Redux/src/App.jsx
+++ 22_09_04-09-Toolkit-Redux/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
@@ -9,7 +8,7 @@ function App() {
     <div className="App">
       <div>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          
         </a>
       </div>
       <h1>Vite + React</h1>
