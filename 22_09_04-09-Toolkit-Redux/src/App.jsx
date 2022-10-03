a1547e8863d06e837cb467acf554bd42018a097a
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-10-02 20:37:55 -0500

    useSelector, dispatch, actions button

--- 22_09_04-09-Toolkit-Redux/src/App.jsx
+++ 22_09_04-09-Toolkit-Redux/src/App.jsx
@@ -1,20 +1,22 @@
-import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux' //** Los hooks que importamos */
 import './App.css'
+import { increment } from './store/slices/counter/counterSlice' //** La funcion que importamos desde el counterSlice */
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const { counter } = useSelector( state => state.counter ) //** Para leer algo del store/selector(seleccionador) */
+  const dispatch = useDispatch() //** Para hacer el dispatch lo inicializamos */
 
   return (
     <div className="App">
       <div>
         <a href="https://reactjs.org" target="_blank">
-          
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => dispatch( increment()) }> {/* Le doy funcionalidad al boton, llamando una funcion del counterSlice llamando la funcion definida arriba */}
+          count is { counter } {/* Esto esta dentro del boton, es una leyenda */}
         </button>
       </div>
     </div>
