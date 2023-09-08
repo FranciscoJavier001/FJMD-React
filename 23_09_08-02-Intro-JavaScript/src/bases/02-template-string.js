ab8b23c0be9b24b408bfd9a211842cb3a57ce81b
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-09-08 15:27:08 -0600

    Annotations and New Script

--- 23_09_08-02-Intro-JavaScript/src/bases/02-template-string.js
+++ 23_09_08-02-Intro-JavaScript/src/bases/02-template-string.js
@@ -1,15 +1,14 @@
-
-
-const nombre = 'Francisco';
-const apellido = 'Martinez';
+const nombre = "Francisco"; //* Defino una variable */
+const apellido = "Martinez"; //* Defino una variable */
 
 // const nombreCompleto = nombre + ' ' + apellido;
-const nombreCompleto = `${nombre} ${apellido}`;
+const nombreCompleto = `${nombre} ${apellido}`; //* Le asigno a esta constante unos parametros definidos arriba (Variables integras) */
 
-console.log(nombreCompleto);
+console.log(nombreCompleto); //* Muestro en consola el valor de esta variable */
 
-function getSaludo(nombre){
-    return 'Hola ' + nombre;
+function getSaludo(nombre) {
+  //* Creo una funcion llamada getSaludo que recibe un parametro */
+  return "Hola " + nombre; //* Retorna al final un hola y luego el nombre */
 }
 
-console.log(`Este es un texto: ${getSaludo(nombre)}`);
+console.log(`Este es un texto: ${getSaludo(nombre)}`); //* Muestro en consola un texto y llamo la funcion llamada getSaludo */
--- 23_09_08-02-Intro-JavaScript/src/main.jsx
+++ 23_09_08-02-Intro-JavaScript/src/main.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 // import "./bases/00-test.js"; //* Mando llamar este Script */
-import "./bases/01-const-let.js"; //* Ejecute este Script */
+// import "./bases/01-const-let.js"; //* Ejecute este Script */
+import "./bases/02-template-string.js"; //* Ejecute este Script */
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
