ed00200ffa2693e503d623d0c15ca0c5d0279512
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-09-08 15:55:18 -0600

    Annotations

--- 23_09_08-02-Intro-JavaScript/src/main.jsx
+++ 23_09_08-02-Intro-JavaScript/src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 // import "./bases/01-const-let.js"; //* Ejecute este Script */
 // import "./bases/02-template-string.js"; //* Ejecute este Script */
 // import "./bases/03-objeto-literal.js"; //* Ejecute este Script */
-import "./bases/04-arreglos.js"; //* Evito Escribir */
+import "./bases/04-arreglos.js"; //* Ejecuto este Script */
 // import "./bases"; //* Evito Escribir */
 
 ReactDOM.createRoot(document.getElementById("root")).render(
