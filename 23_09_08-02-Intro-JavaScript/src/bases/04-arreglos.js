ca6fe3ff3057a5002b25f904240e1183782ec754
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-09-08 15:54:45 -0600

    Annotations and Running

--- 23_09_08-02-Intro-JavaScript/src/bases/04-arreglos.js
+++ 23_09_08-02-Intro-JavaScript/src/bases/04-arreglos.js
@@ -1,18 +1,21 @@
 // Arreglos en JS
-// const arreglo = new Array(100);
-const arreglo = [1,2,3,4];
-// arreglo.push(1);
-// arreglo.push(2);
-// arreglo.push(3);
-// arreglo.push(4);
 
-let arreglo2 = [...arreglo, 5];
+// const arreglo = new Array(100); //* Creo un arreglo que tenga 100 espacios (Inicia en 0, sera 101) */
+const arreglo = [1, 2, 3, 4]; //* Creo un arreglo con numeros */
+// arreglo.push(1); //* Le agrego un 1 */
+// arreglo.push(2); //* Le agrego un 2 */
+// arreglo.push(3); //* Le agrego un 3 */
+// arreglo.push(4); //* Le agrego un 4 */
+// arreglo.pop(); //* Elimina un espacio, el ultimo */
+
+let arreglo2 = [...arreglo, 5]; //* Creo otro arreglo igual al 1, pero le agrego un 5 */
 
 // Map crea un nuevo arreglo
-const arreglo3 = arreglo2.map(function(numero) {
-    return numero*2;
+const arreglo3 = arreglo2.map((numero) => {
+  //* Defino un nuevo arreglo que va a ser igual al 2, que mapea (es igual) al 2, pero recibe un parametro */
+  return numero * 2; //* El parametro que recibio, sera todo que se multiplique por 2 */
 });
 
-console.log(arreglo);
-console.log(arreglo2);
-console.log(arreglo3);
+console.log(arreglo); //* Muestro en consola el arreglo 1 */
+console.log(arreglo2); //* Muestro en consola el arreglo 2 */
+console.log(arreglo3); //* Muestro en consola el arreglo 3 */
--- 23_09_08-02-Intro-JavaScript/src/main.jsx
+++ 23_09_08-02-Intro-JavaScript/src/main.jsx
@@ -4,7 +4,8 @@ import App from "./App.jsx";
 // import "./bases/00-test.js"; //* Mando llamar este Script */
 // import "./bases/01-const-let.js"; //* Ejecute este Script */
 // import "./bases/02-template-string.js"; //* Ejecute este Script */
-import "./bases/03-objeto-literal.js"; //* Ejecute este Script */
+// import "./bases/03-objeto-literal.js"; //* Ejecute este Script */
+import "./bases/04-arreglos.js"; //* Evito Escribir */
 // import "./bases"; //* Evito Escribir */
 
 ReactDOM.createRoot(document.getElementById("root")).render(
