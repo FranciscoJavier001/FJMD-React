5c501f35cc9398df1e9faea63911a9b0d9477e90
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-09-08 15:28:43 -0600

    Tip and New Script

--- 23_09_08-02-Intro-JavaScript/src/main.jsx
+++ 23_09_08-02-Intro-JavaScript/src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 // import "./bases/00-test.js"; //* Mando llamar este Script */
 // import "./bases/01-const-let.js"; //* Ejecute este Script */
-import "./bases/02-template-string.js"; //* Ejecute este Script */
+// import "./bases/02-template-string.js"; //* Ejecute este Script */
+import "./bases/03-objeto-literal.js"; //* Ejecute este Script */
+// import "./bases"; //* Evito Escribir */
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
