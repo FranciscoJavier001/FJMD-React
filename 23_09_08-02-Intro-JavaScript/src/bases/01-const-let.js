0ef49f5161ad7aa67e907640e2586b605d64cf9b
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-09-08 15:22:19 -0600

    Annotations and New Script

--- 23_09_08-02-Intro-JavaScript/src/bases/01-const-let.js
+++ 23_09_08-02-Intro-JavaScript/src/bases/01-const-let.js
@@ -1,19 +1,20 @@
-
 // Variables y Constantes
 
-const nombre = 'Francisco';
-const apellido = 'Martinez';
+const nombre = "Francisco"; //* Defino una variable */
+const apellido = "Martinez"; //* Defino una variable */
 
-let valorDado = 5;
-valorDado = 4;
+let valorDado = 5; //* Le asigno una variable */
+valorDado = 4; //* Como es let puedo modificar esta variable */
 
-console.log(nombre, apellido, valorDado);
+console.log(nombre, apellido, valorDado); //* Muestro en Consola */
 
 // var No se debe de usar
-if(true){
-    const nombre = 'Peter'
+if (true) {
+  //* Para que esta condicion siempre entre */
+  const nombre = "Peter"; //* Cambio el valor de la variable nombre, pero esta fuera del Scope */
 
-    console.log(nombre);
+  console.log(nombre); //* Va a salir el nombre de Peter */
 }
 
-console.log(valorDado);
+console.log(valorDado); //* Va a salir el Valor asignado al ultimo de esta variable */
+console.log(nombre); //* Sale Francisco porque esta definido fuera del Scope */
