02d891f0d863bfc3d11127984c5a6ad7d5906d12
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-09-16 23:38:30 -0600

    Homework

--- 23_09_08-02-Intro-JavaScript/src/bases/07-deses-arr.js
+++ 23_09_08-02-Intro-JavaScript/src/bases/07-deses-arr.js
@@ -1,22 +1,22 @@
-const personajes = ['Goku', 'Vegeta', 'Trunks'];
-const [, , p3] = personajes;
-console.log(p3);
+const personajes = ["Goku", "Vegeta", "Trunks"]; //* Creo un arreglo de arreglos */
+const [, , p3] = personajes; //* Defino caracteristicas o arreglos de las variables que estan en personajes */
+console.log(p3); //* Solo va a salir Trunks */
 
-const retornaArreglo = () =>{
-    return ['ABC', 123];
-}
+//*  */ //*  */ //*  */ //*  */ //*  */ //*  */ //*  */ //*  */ //*  */ //*  */ //*  */ //*  */ //*  */ //*  */ //*  */ //*  */ //*  */ //*  */ //*  */ //*  */
 
-const [letras, numeros] = retornaArreglo();
-console.log(letras, numeros);
+const retornaArreglo = () => {
+  // Funcion que no recibe parametros, que retorna in String y unos numeros, sin return no me retorna nada */
+  return ["ABC", 123]; //* Regresame eso en consola, lo que defina */
+};
+
+const [letras, numeros] = retornaArreglo(); //* Creo como se llaman las caracteristocas del objeto retornaArreglo */
+console.log(letras, numeros); //* Muestro en consola las variables que asigne el numerode 0 a donde sea *
+
+//*  */ //*  */ //*  */ //*  */ //*  */ //*  */ //*  */ //*  */ //*  */ //*  */ //*  */ //*  */ //*  */ //*  */ //*  */ //*  */ //*  */ //*  */ //*  */ //*  */
 
 // Tarea
 // 1. El primer valor del arr se llamara nombre
 // 1. El segundo valor del arr se llamara setNombre
-const useState = (valor) => {
-    return [valor, ()=>{ console.log('Hola Mundo')}];
-}
 
-const [nombre, setNombre] = useState('Goku');
-
-console.log(nombre);
-setNombre();
+//* Defino una variable que reciba un elemento */
+//* Y muestra en consola hola mundo */
